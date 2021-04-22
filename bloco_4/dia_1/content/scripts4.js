fe9b7c2e559c0c67dc27fce0dd020fea3a8f67eb
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-22 17:51:39 -0300

    parte 4 do conteudo

---
+++ bloco_4/dia_1/content/scripts4.js
@@ -0,0 +1,11 @@
+let trybe = 17;
+
+if (trybe >= 14 && trybe <= 14.50){
+    console.log('esquenta')
+}else if (trybe >= 16.20 && trybe <= 18){
+    console.log('Aula ao vivo')
+}else if (trybe >= 19.40 && trybe <= 20){
+    console.log('fechamento')
+}else {
+    console.log('fora dos momentos sícronos')
+}
