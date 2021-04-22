e9293958be24d7d9f0b826a7ae38537a2e7bcd2e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-22 18:23:17 -0300

    exercicio 1 - multiplicacao

--- bloco_4/dia_1/exercices/scripts1.js
+++ bloco_4/dia_1/exercices/scripts1.js
@@ -6,8 +6,15 @@
 // console.log(soma);
 
 // --- SUBTRAÇÃO:
-let a = 4;
-let b = 2;
+// let a = 4;
+// let b = 2;
 
-let subtracao = a - b;
-console.log(subtracao);
+// let subtracao = a - b;
+// console.log(subtracao);
+
+// ---MULTIPLICAÇÃO:
+let a = 3;
+let b = 3;
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
