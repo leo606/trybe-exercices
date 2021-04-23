5d7ece37874f6e23eeed01869009176baeac666f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-23 15:31:49 -0300

    exercicio conteurdo - for

---
+++ bloco_4/dia_2/content/scripts2.js
@@ -0,0 +1,11 @@
+let marcasCarros = ['saab', 'volvo', 'BMW'];
+
+for (let index = 0; index < marcasCarros.length; index += 1){
+    console.log(marcasCarros[index]);
+}
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1){
+    console.log(groceryList[index]);
+}
