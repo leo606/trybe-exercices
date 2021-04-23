9fe03add1e80e0fd1f69d1b50541864159d92d6d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-23 18:18:50 -0300

    conteudo aula - megasena

---
+++ bloco_4/dia_2/content/scripts4.js
@@ -0,0 +1,13 @@
+let firstNumber = Math.ceil((Math.random() * 60));
+let secondtNumber = Math.ceil((Math.random() * 60));
+let thirdNumber = Math.ceil((Math.random() * 60));
+let fourthNumber = Math.ceil((Math.random() * 60));
+let fifthNumber = Math.ceil((Math.random() * 60));
+let sixthNumber = Math.ceil((Math.random() * 60));
+
+megaSenaNumbers = [firstNumber, secondtNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
+playerNumbers = [44, 12, 63, 53, 8];
+
+for (let index = 0; index < megaSenaNumbers; index += 1){
+    if (playerNumbers[index] ===)
+}
