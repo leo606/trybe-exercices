57de9a340bf19591d2821d566aa637edb793bee1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-25 23:19:58 -0300

    exercicio megasena - adiciona array jogos

--- bloco_4/dia_2/content/scripts4.js
+++ bloco_4/dia_2/content/scripts4.js
@@ -6,18 +6,22 @@ let fifthNumber = Math.ceil((Math.random() * 60));
 let sixthNumber = Math.ceil((Math.random() * 60));
 
 let megaSenaNumbers = [fifthNumber, secondtNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
-let playerNumbers = [12, 44, 8, 63, 9, 53];
+let playerNumbers1 = [1, 2, 3, 4, 5, 6];
+let playerNumbers2 = [7, 8, 9, 10, 11, 12];
+let playerNumbers3 = [11, 21, 31, 41, 51, 16];
+let playerNumbers = [playerNumbers1, playerNumbers2, playerNumbers3];
 let numbersHits = 0;
 
-for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1){
-    for (let indexGame = 0; indexGame < playerNumbers.length; indexGame += 1){
-        if (playerNumbers[indexGame] == megaSenaNumbers[indexMega]){
-            numbersHits += 1;
+for (let games = 0; games < playerNumbers.length; games += 1){
+    for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1){
+        for (let indexGame = 0; indexGame < playerNumbers[games].length; indexGame += 1){
+            if (playerNumbers[games] == megaSenaNumbers[indexMega]){
+                numbersHits += 1;
+            }
         }
     }
+    console.log(`jogada ${games + 1}`);
+    console.log(`mega sena ${megaSenaNumbers}`);
+    console.log(`jogo ${playerNumbers[games]}`);
+    console.log(`acertos ${numbersHits}`)
 }
-
-
-console.log(numbersHits)
-console.log(`sorteio ${megaSenaNumbers}`)
-console.log(`jogo    ${playerNumbers}`)
