f7dac670e22baaa8b05ea1ad95eab43bd0e8b8ec
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-26 11:17:22 -0300

    exercicio conteudo megasena - verifica array de varios games

--- bloco_4/dia_2/content/scripts4.js
+++ bloco_4/dia_2/content/scripts4.js
@@ -1,27 +1,32 @@
-let firstNumber = Math.ceil((Math.random() * 60));
-let secondtNumber = Math.ceil((Math.random() * 60));
-let thirdNumber = Math.ceil((Math.random() * 60));
-let fourthNumber = Math.ceil((Math.random() * 60));
-let fifthNumber = Math.ceil((Math.random() * 60));
-let sixthNumber = Math.ceil((Math.random() * 60));
+let megaSenaNumbers = [];
 
-let megaSenaNumbers = [fifthNumber, secondtNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
-let playerNumbers1 = [1, 2, 3, 4, 5, 6];
-let playerNumbers2 = [7, 8, 9, 10, 11, 12];
-let playerNumbers3 = [11, 21, 31, 41, 51, 16];
-let playerNumbers = [playerNumbers1, playerNumbers2, playerNumbers3];
-let numbersHits = 0;
+let firstNumber = Math.ceil(Math.random() * 60);
+let secondNumber = Math.ceil(Math.random() * 60);
+let thirdNumber = Math.ceil(Math.random() * 60);
+let fourthNumber = Math.ceil(Math.random() * 60);
+let fifthNumber = Math.ceil(Math.random() * 60);
+let sixthtNumber = Math.ceil(Math.random() * 60);
 
-for (let games = 0; games < playerNumbers.length; games += 1){
+megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthtNumber];
+let player1Game = [44, 56, 60, 23, 18, 45];
+let player2Game = [45, 54, 59, 24, 20, 40];
+let player3Game = [39, 51, 47, 25, 4, 54];
+let playerGame = [player1Game, player2Game, player3Game];
+
+for (let indexGames = 0; indexGames < playerGame.length; indexGames += 1) {
+    let numberOfHits = 0;
     for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1){
-        for (let indexGame = 0; indexGame < playerNumbers[games].length; indexGame += 1){
-            if (playerNumbers[games] == megaSenaNumbers[indexMega]){
-                numbersHits += 1;
-            }
-        }
-    }
-    console.log(`jogada ${games + 1}`);
-    console.log(`mega sena ${megaSenaNumbers}`);
-    console.log(`jogo ${playerNumbers[games]}`);
-    console.log(`acertos ${numbersHits}`)
-}
+        for (let indexGame = 0; indexGame < playerGame[indexGames].length; indexGame += 1){
+            if (playerGame[indexGames][indexGame] === megaSenaNumbers[indexMega]){
+                numberOfHits += 1;
+            };
+        };
+    };
+    console.log(`MEGASENA      ${megaSenaNumbers}`);
+    console.log(`PLAYER GAME   ${playerGame[indexGames]}`);
+    console.log(`ACERTOS       ${numberOfHits}`);
+    
+};
+
+
+
