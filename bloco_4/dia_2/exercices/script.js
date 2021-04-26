378ad17810eae7181427539cee34da17e296db63
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-26 15:18:16 -0300

    exercicio conteudo - adicionar descricao de algoritimo nos exercicios do dia anterior

--- bloco_4/dia_2/exercices/script.js
+++ bloco_4/dia_2/exercices/script.js
@@ -2,12 +2,23 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // EXERCICIO 1:
 // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// ---ALGORITIMO---
+// criar array
+// criar condicao de percorrer o array
+// criar output para cada identação
+
 for (let index = 0; index < numbers.length; index += 1){
     // console.log(numbers[index]);
 }
 
 // EXERCICIO 2:
 // Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// ---ALGORITIMO---
+// criar Array
+// criar variavel para armazenar a soma
+// criar condicao para percorrer o array adicionando cada valor a variavel soma
+// output da soma
+
 let soma = 0;
 for (let index2 = 0; index2 < numbers.length; index2 += 1){
     soma = soma + numbers[index2];
@@ -16,11 +27,24 @@ console.log(`A soma é ${soma}`);
 
 // EXERCICIO 3:
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// ---ALGORITIMO---
+// criar array
+// criar variavel soma
+// criar condicao para percorrer o array
+// a cada loop da condicao somar em soma
+// dividiar a soma pela quantidade de valores
+// output do resultado
+
 let mediaAritmetica = soma / numbers.length;
 console.log(`A média aritmética é ${mediaAritmetica}`);
 
 // EXERCICIO 4:
 // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// ---ALGORITIMO---
+// usar resultado do ex anterior como valor da condicao
+// criar condicao para verificar se o valor que 20
+
+
 if (mediaAritmetica > 20) {
     console.log('Valor maior que 20');
 }else {
@@ -29,6 +53,13 @@ if (mediaAritmetica > 20) {
 
 // EXERCICIO 5:
 // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+---ALGORITIMO---
+criar array
+criar variavel para armazenar o maior numero
+criar condicao para percorrer o array e verifivar se cada valor é maior que o maior numero
+se for maior substituir o valor da variavel de maior numero pelo maior numeoro encontrado
+al termino da condicao de percorrer, imprimir a variavel maior numero
+
 let maiorValor = 0;
 for (let index3 = 0; index3 < numbers.length; index3 += 1){
     if (numbers[index3] > maiorValor){
