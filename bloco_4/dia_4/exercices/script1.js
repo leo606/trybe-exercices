f50c1a7495d6b6a8bd35f2eeac8a6b2437dbe882
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-27 18:15:28 -0300

    exercicios dia 4.4 - exercicio 4 e 5

--- bloco_4/dia_4/exercices/script1.js
+++ bloco_4/dia_4/exercices/script1.js
@@ -5,9 +5,37 @@ let info = {
   recorrente: 'Sim',
 };
 
-console.log('Bem vinda, ' + info.personagem);
-console.log(info);
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Chistmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+}
+
+// console.log('Bem vinda, ' + info.personagem);
+// console.log(info);
+
+// // mostrar chaves:
+// for (index in info){
+//   console.log(index);
+// };
+
+// // mostrar valores das chaves:
+
+// for (index in info){
+//   console.log(info[index]);
+// }
+
+// imprimir os dois objetos juntos de acordo com as chaves:
 
 for (index in info){
-  console.log(index);
+  if (info[index] === info.recorrente){
+    if (info[index] === info2[index]){
+      console.log('Ambos são recorrentes');
+    }else{
+      console.log(info[index] + ' e ' + info2[index]);
+    }
+  }else{
+    console.log(info[index], info2[index]);
+  };
 };
