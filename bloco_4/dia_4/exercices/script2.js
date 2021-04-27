9c03e25d982dc999c82971ca352de20e58748a19
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-27 20:17:58 -0300

    exercicios dia 4.4 - exercicio 2 de funcoes

--- bloco_4/dia_4/exercices/script2.js
+++ bloco_4/dia_4/exercices/script2.js
@@ -1,14 +1,43 @@
-function verificarPalindromo(word) {
+// EXERCICIO 1: 
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function checkPalindrome(word) {
   let inputWord = word;
   let invertedWord = '';
   for (let index = (inputWord.length - 1); index >= 0; index -= 1) {
     invertedWord = invertedWord + inputWord[index];
   };
-  if (invertedWord == inputWord){
+  if (invertedWord == inputWord) {
     return true;
-  }else{
+  } else {
     return false;
   };
 };
 
-console.log(verificarPalindromo('lel'));
+// console.log(checkPalindrome('454'));
+
+// EXERCICIO 2: 
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function maiorValor (numeros) {
+  let array = numeros;
+  let maiorValor = 0;
+  for (let index in numeros){
+    if (numeros[index] > maiorValor){
+      maiorValor = numeros[index];
+    };
+  };
+  return numeros.indexOf(maiorValor);
+};
+
+console.log(maiorValor([999, 1001, 1002]));
+
+// let numeros = [33, 91, 90, 85];
+// let maiorValor = 0;
+
+// for (let index in numeros) {
+//   if (numeros[index] > maiorValor) {
+//     maiorValor = numeros[index];
+//   };
+// };
+// console.log(maiorValor);
