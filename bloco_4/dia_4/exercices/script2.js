23fb43bff704a46b7681d52c70a1b8709d6f9b2b
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-28 12:53:09 -0300

    exercicios dia 4.4 - exercicio 5 de funcoes

--- bloco_4/dia_4/exercices/script2.js
+++ bloco_4/dia_4/exercices/script2.js
@@ -13,51 +13,70 @@ function checkPalindrome(word) {
     return false;
   };
 };
-
 // console.log(checkPalindrome('454'));
 
 // EXERCICIO 2: 
 // Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-function maiorValor (numeros) {
+function maiorValor(numeros) {
   let array = numeros;
   let maiorValor = 0;
-  for (let index in numeros){
-    if (numeros[index] > maiorValor){
+  for (let index in numeros) {
+    if (numeros[index] > maiorValor) {
       maiorValor = numeros[index];
     };
   };
   return numeros.indexOf(maiorValor);
 };
-
 // console.log(maiorValor([999, 1001, 1002]));
 
 // EXERCICIO 3:
 // Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
-function menorValor(numeros){
+function menorValor(numeros) {
   let menorValor = numeros[0];
-  for (index in numeros){
-    if (numeros[index] < menorValor){
+  for (index in numeros) {
+    if (numeros[index] < menorValor) {
       menorValor = numeros[index];
     };
   }
   return numeros.indexOf(menorValor);
 };
-
 // console.log(menorValor([1, 6, 90, 3, -1, -99]))
 
 // EXERCICIO 4:
 // Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
-function longestName (nomes){
+function longestName(nomes) {
   let longName = '';
-  for (index in nomes){
-    if (nomes[index].length > longName.length){
+  for (let index in nomes) {
+    if (nomes[index].length > longName.length) {
       longName = nomes[index];
     };
   };
   return longName;
 };
+// console.log(longestName(['Joaaaaaaaaaaaaaasé', 'Lucasssssss', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 
-console.log(longestName(['Joaaaaaaaaaaaaaasé', 'Lucasssssss', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+// EXERCICIO 5:
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+function mostRepeated(numbers) {
+  let mostRepeatedNumber = 0;
+  let repeats = 0;
+  let numberRepeat = 0;
+  for (let index in numbers) {
+    for (let indexNumber = 0; indexNumber < numbers.length; indexNumber += 1) {
+      if (numbers[index] === numbers[indexNumber]) {
+        numberRepeat += 1;
+      };
+    };
+    if (numberRepeat >= repeats){
+      mostRepeatedNumber = numbers[index];
+      repeats = numberRepeat;
+    }
+    numberRepeat = 0;
+  };
+  return mostRepeatedNumber;
+}
+console.log(mostRepeated([2, 3, 2, 5, 8, 8, 8, 8, 2, 3]));
