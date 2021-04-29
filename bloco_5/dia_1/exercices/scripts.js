0e92891878beb763b5c0f8278df3033238283e5e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-29 18:02:54 -0300

    exercicio 1

---
+++ bloco_5/dia_1/exercices/scripts.js
@@ -0,0 +1,5 @@
+// EXERCICIO 1:
+function mudarParagrafo (paragrafo, text){
+  paragrafo.innerHTML = text;
+}
+mudarParagrafo(document.getElementById('paragrafo'), 'daqui a dois anos vou estar codando demais');
