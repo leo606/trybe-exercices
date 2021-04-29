e6344d4c14bba2bd545d8485c0cae91049c1afae
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-29 19:40:02 -0300

    exercicio 4 e 5

--- bloco_5/dia_1/exercices/scripts.js
+++ bloco_5/dia_1/exercices/scripts.js
@@ -10,7 +10,24 @@ function mudarCorMain(div){
 }
 mudarCorMain(document.getElementsByClassName('main-content')[0]);
 
+// EXERCICIO 3:
 function mundarCorCenter (div){
   div.style.backgroundColor = 'whitesmoke';
 }
 mundarCorCenter(document.getElementsByClassName('center-content')[0])
+
+// EXERCICIO 4:
+function corrigeH1 (heading){
+  heading.innerHTML = 'Exercício 5.1 - JavaScript';
+}
+corrigeH1(document.getElementsByClassName('title')[0]);
+
+// EXERCICIO 5:
+function toUpper (paragrafo){
+  for (let index = 0; index < paragrafo.length; index += 1){    
+    let text = paragrafo[index].innerHTML;
+    let textupper = text.toUpperCase();
+    paragrafo[index].innerHTML = textupper;
+  }
+}
+toUpper(document.getElementsByTagName('p'));
