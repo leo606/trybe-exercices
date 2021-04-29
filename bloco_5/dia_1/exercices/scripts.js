0438c839dcfe6764f815dd4a9b4169c54ba509d4
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-29 18:07:39 -0300

    exercicio 2

--- bloco_5/dia_1/exercices/scripts.js
+++ bloco_5/dia_1/exercices/scripts.js
@@ -3,3 +3,10 @@ function mudarParagrafo (paragrafo, text){
   paragrafo.innerHTML = text;
 }
 mudarParagrafo(document.getElementById('paragrafo'), 'daqui a dois anos vou estar codando demais');
+
+// EXERCICIO 2:
+function mudarCorMain(div){
+  div.style.backgroundColor = 'rgb(76,164,109)'
+}
+mudarCorMain(document.getElementsByClassName('main-content')[0]);
+
