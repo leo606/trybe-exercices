8843d31c49968ddc21e31371b81779edf2f486a1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-29 10:58:53 -0300

    dia 5.1 - conteudo

---
+++ bloco_5/dia_1/content/scripts.js
@@ -0,0 +1,12 @@
+document.getElementById("page-title").innerText = 'Inglorious Basterds';
+document.getElementById('second-paragraph').innerText = "You know somethin', Utivich? I think this just might be my masterpiece.";
+document.getElementById("subtitle").innerText = 'Quentin Tarantino'
+
+let paragrafos = document.getElementsByClassName("paragrafo");
+paragrafos[0].style.color = 'orange';
+paragrafos[1].style.color = 'green';
+console.log(paragrafos[0]);
+
+document.getElementsByTagName('h4')[0].style.color = 'yellow';
+
+console.log(sub[0]);
