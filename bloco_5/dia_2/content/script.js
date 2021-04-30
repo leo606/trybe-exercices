ece5dbbe90f8d4d961f83b6ab8fc820664387953
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-30 15:12:56 -0300

    exercicios do conteudo dia 5.2 - parte 2

--- bloco_5/dia_2/content/script.js
+++ bloco_5/dia_2/content/script.js
@@ -11,3 +11,25 @@ document.querySelector('#elementoOndeVoceEsta').nextSibling;
 document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 
 document.getElementById('pai').lastElementChild.previousElementSibling;
+
+// creteElement() -- cria elementos HTML
+// appendChild() -- adiciona no HTML
+
+let pai = document.querySelector('#pai');
+let divIrmao = document.createElement('div');
+divIrmao.innerHTML = 'pipipopo'
+pai.appendChild(divIrmao);
+
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+let divFilho = document.createElement('div')
+divFilho.innerHTML = 'rsrs'
+elementoOndeVoceEsta.appendChild(divFilho);
+
+// document.getElementById('elementoOndeVoceEsta').appendChild(document.createElement('div').innerHTML = 'hohoh')
+
+let primeiroFilhoDoFilho = document.querySelector('#elementoOndeVoceEsta').firstElementChild;
+let divPrimeiroFilho = document.createElement('div')
+divPrimeiroFilho.innerHTML = 'esse é um filho do primeiro filho do filho'
+primeiroFilhoDoFilho.appendChild(divPrimeiroFilho);
+
+document.querySelector('#primeiroFilhoDoFilho div').parentElement.parentElement.parentElement.lastElementChild.previousElementSibling.previousElementSibling;
