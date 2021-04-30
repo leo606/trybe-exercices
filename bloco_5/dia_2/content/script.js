9ef71db0f258c2be8c000e59a5039a5488b87656
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-30 11:08:51 -0300

    exercicios do conteudo dia 5.2 - parte 1

---
+++ bloco_5/dia_2/content/script.js
@@ -0,0 +1,13 @@
+document.getElementById('elementoOndeVoceEsta').parentElement.style.backgroundColor = 'orange';
+
+document.getElementById('elementoOndeVoceEsta').firstElementChild.innerHTML = 'Esse é o primeiro filho do filho'
+
+document.getElementById('pai').firstElementChild;
+
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
+
+document.querySelector('#elementoOndeVoceEsta').nextSibling;
+
+document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+
+document.getElementById('pai').lastElementChild.previousElementSibling;
