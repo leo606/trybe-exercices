28da5265c010a7d83267c0a3d0942a61dbd092c3
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-03 18:29:51 -0300

    conteudo dia 5.3 - todos os exs

--- bloco_5/dia_3/content/main.js
+++ bloco_5/dia_3/content/main.js
@@ -11,18 +11,18 @@ const myWebpage = document.getElementById('mySpotrybefy');
 divUm.addEventListener('click', recebeClick);
 divDois.addEventListener('click', recebeClick);
 divTres.addEventListener('click', recebeClick);
-function recebeClick(origem){
-  if (origem.target == divUm){
+function recebeClick(origem) {
+  if (origem.target == divUm) {
     divDois.classList.remove('tech');
     divTres.classList.remove('tech');
     divUm.className = 'tech';
   }
-  if (origem.target == divDois){
+  if (origem.target == divDois) {
     divUm.classList.remove('tech');
     divTres.classList.remove('tech');
     divDois.className = 'tech';
   }
-  if (origem.target == divTres){
+  if (origem.target == divTres) {
     divUm.classList.remove('tech');
     divDois.classList.remove('tech');
     divTres.className = 'tech';
@@ -32,13 +32,34 @@ function recebeClick(origem){
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('keyup', inputToTech)
+input.addEventListener('blur', clearInput)
+function inputToTech(origem) {
+  let pInTech = document.querySelector('.container .tech');
+  pInTech.innerHTML = input.value;
+}
+function clearInput() {
+  input.value = '';
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', redirect)
+function redirect() {
+  window.location.href = 'https://leo606.github.io'
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseenter', changeTop3ColorIn)
+myWebpage.addEventListener('mouseleave', changeTop3ColorOut)
+function changeTop3ColorIn() {
+  myWebpage.style.color = 'orange';
+}
+function changeTop3ColorOut() {
+  myWebpage.style.color = 'white'
+}
 
 // Segue abaixo um exemplo do uso de event.target:
 
