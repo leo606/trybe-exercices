ec3ef2849ca94b05be9dc85f0174902d93883165
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-19 19:47:16 -0300

    day 7.1 - part 2 - ex 1- add comment

--- bloco_7/dia_1/exercices/script3.js
+++ bloco_7/dia_1/exercices/script3.js
@@ -9,4 +9,18 @@ const fatorial = (num) => {
   }
   return fatorialResult
 }
-console.log(fatorial(4));
+console.log(fatorial(6));
+
+// ------RECURSIVO DO SITE https://backefront.com.br/como-fazer-fatorial-recursivo-javascript/------
+
+// const fatorial = (numero) => {
+//   if (numero < 0) {
+//     return false;
+//   }
+//   return numero === 0 ? 1 : numero * fatorial(numero - 1);
+// };
+
+// console.log(fatorial(5));
+// Resultado esperado: 120
+
+
