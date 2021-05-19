d47436891a509c4ae745862017f1687405c2d723
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-19 13:39:16 -0300

    content 7.1 - 3

--- bloco_7/dia_1/content/script.js
+++ bloco_7/dia_1/content/script.js
@@ -7,10 +7,18 @@
 // }
 // userInfo();
 
-if (true) {
-  // inicio do escopo do if
-  const userAge = "20";
-  console.log(userAge); // 20
-  // fim do escopo do if
-}
-console.log(userAge, 'fora'); // 20
+// if (true) {
+//   // inicio do escopo do if
+//   const userAge = "20";
+//   console.log(userAge); // 20
+//   // fim do escopo do if
+// }
+// console.log(userAge, 'fora'); // 20
+
+let favoriteLanguage = "Javascript";
+favoriteLanguage = "Python";
+console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = "Machine learning";
+favoriteTechnology = "Facial recognition";
+console.log(favoriteTechnology); // Facial recognition
