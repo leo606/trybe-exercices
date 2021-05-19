ecf145a40151b554ecf99555a3ab26c75d9b25f4
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-19 17:57:16 -0300

    day 7.1 - part 1, ex 1.2

--- bloco_7/dia_1/exercices/script1.js
+++ bloco_7/dia_1/exercices/script1.js
@@ -1,12 +1,11 @@
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
+    console.log(`${ifScope} ótimo, fui utilizada no escopo!`);
   } else {
     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
 }
 
-testingScope(false);
+testingScope(true);
