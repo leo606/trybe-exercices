088c5be1a39fab67d5fddf10a20929d9063e3176
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-20 19:59:44 -0300

    day 7.2 - exercices - part 2 ex 8

--- bloco_7/dia_2/exercices/parte2.js
+++ bloco_7/dia_2/exercices/parte2.js
@@ -20,14 +20,14 @@ const lesson3 = {
 
 // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros,
 // sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
-function addTurno(obj, key, value){
+function addTurno(obj, key, value) {
   obj[key] = value;
 };
 addTurno(lesson2, 'turno', 'manhã');
 // console.log(lesson2);
 
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-function listKeys(obj){
+function listKeys(obj) {
   Object.keys(obj).forEach(key => console.log(key));
 }
 // listKeys(lesson2);
@@ -71,7 +71,7 @@ function objValues(obj) {
 // */
 
 function allLessons() {
-  return Object.assign({}, {lesson1, lesson2, lesson3})
+  return Object.assign({}, { lesson1, lesson2, lesson3 })
 }
 const allLessonsObj = allLessons();
 // console.log(allLessonsObj);
@@ -85,7 +85,19 @@ function numeroEstudantes() {
 
 
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
-function getKey(obj, position){
+function getKey(obj, position) {
   return obj[Object.keys(obj)[position]];
 }
-console.log(getKey(lesson1, 0));
+// console.log(getKey(lesson1, 0));
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros
+// sendo eles: o objeto, o nome da chave e o valor da chave.
+function checkKeyValue(obj, key, value) {
+  if (obj[key] === value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkKeyValue(lesson1, 'materia', 'sd'));
