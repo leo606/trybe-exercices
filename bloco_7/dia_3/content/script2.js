fdf7a39ccfba86336a4b06544bbb98996302366a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-21 10:57:19 -0300

    day 7.3 - content examples 2

--- bloco_7/dia_3/content/script2.js
+++ bloco_7/dia_3/content/script2.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 
-const sum = (num1, num2) => num1 + num2;
+// const sum = (num1, num2) => num1 + num2;
 
-const result = sum(1, 3);
+// const result = sum(1, 3);
 
-assert.ok(result === 3, 'é tres');
-assert.strictEqual(result, 3, 'é tres ta');
-assert.notStrictEqual(result, 4, 'nao quatro');
+// assert.ok(result === 3, 'é tres');
+// assert.strictEqual(result, 3, 'é tres ta');
+// assert.notStrictEqual(result, 4, 'nao quatro');
+
+const list1 = [1, 2, 3, 4, 5, 6];
+const list2 = [1, 2, 3, 4, 5, 6, 7];
+
+assert.deepStrictEqual(list1, list2, 'não sao iguais');
