53d759ee46efa3b8003d783ed0dd72463739528c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-22 10:30:46 -0300

    day 7.3 - exercices - part 1 - ex 4

--- bloco_7/dia_3/exercices/part1_f2.js
+++ bloco_7/dia_3/exercices/part1_f2.js
@@ -65,3 +65,37 @@ assert.notDeepStrictEqual(testArray2, [1, 2, 3, 4, 5, 6], 'testArray2 não sofre
 
 // Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4], 'myRemoveWithoutCopy alterou o array recebendo um índice inexistente no array');
+
+// A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 ,
+// retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 ,
+// retorna o próprio número caso não seja divisível por 3 ou 5 e
+// retorna false caso num não seja um número
+
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz', '15 é divisível por 3 e 5');
+
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(9), 'fizz', '9 é divisível só por 3');
+
+// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(20), 'buzz', '20 é divisível só por 3');
+
+// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(16), 16, '16 não é divisível por 3 ou 5');
+
+// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz('test'), false, 'myFizzBuzz não retornou false ao receber uma string');
