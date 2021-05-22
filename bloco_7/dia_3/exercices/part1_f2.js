f21a7f02c3e7570f9d923e30473765a522de2744
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-22 10:15:09 -0300

    day 7.3 - exercices - part 1 - ex 3

--- bloco_7/dia_3/exercices/part1_f2.js
+++ bloco_7/dia_3/exercices/part1_f2.js
@@ -26,10 +26,42 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], 'não retornou o ar
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'não era pra retornar esse array');
 
 // Verifique se o array passado por parâmetro não sofreu alterações
-let testArray = [1,2,3,4,5,6];
+let testArray = [1, 2, 3, 4, 5, 6];
 myRemove(testArray, 3);
-assert.deepStrictEqual(testArray, [1,2,3,4,5,6], 'o array sofreu alteração');
+assert.deepStrictEqual(testArray, [1, 2, 3, 4, 5, 6], 'o array sofreu alteração');
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
+// A função myRemoveWithoutCopy(arr, item) recebe um array arr e
+// retorna o próprio array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4], 'myRemoveWithoutCopy não retornou o array esperado');
+
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4], 'myRemoveWithoutCopy retornou o mesmo array de entrada');
+
+// Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+let testArray2 = [1, 2, 3, 4, 5, 6];
+myRemoveWithoutCopy(testArray2, 3);
+assert.notDeepStrictEqual(testArray2, [1, 2, 3, 4, 5, 6], 'testArray2 não sofreu alterações');
+
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4], 'myRemoveWithoutCopy alterou o array recebendo um índice inexistente no array');
