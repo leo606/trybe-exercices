df1d64c6110d6b802073810264fd4f6dd08ec5f6
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-25 16:00:39 -0300

    day 8.1 - excercise 2

---
+++ bloco_8/dia_1/exercices/script2.js
@@ -0,0 +1,11 @@
+function checkBet(bet, star){
+  return (bet === star);
+}
+
+function prize(bet, check){
+  const star = Math.ceil(Math.random() * 5);
+  if (check(bet, star)) return `Parabéns você ganhou!`;
+  return 'Tente Novamente'
+}
+
+console.log(prize(3, checkBet));
