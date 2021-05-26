c8360ce85fa6df0f406f8ec407fd1206932a7a45
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-26 15:21:01 -0300

    day 8.2 - content 4

--- bloco_8/dia_2/content/script2.js
+++ bloco_8/dia_2/content/script2.js
@@ -13,3 +13,16 @@ const findNameWithFiveLetters = () => {
 }
 
 console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  let musica = musicas.find(element=> element.id === id);
+  return musica.title;
+}
+
+console.log(findMusic('31031687'))
