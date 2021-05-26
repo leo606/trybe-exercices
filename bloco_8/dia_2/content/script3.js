d9f6c92b72195e1fbe1df52dc97af582e4687fdb
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-26 15:39:37 -0300

    day 8.2 - content 6

--- bloco_8/dia_2/content/script3.js
+++ bloco_8/dia_2/content/script3.js
@@ -4,4 +4,18 @@ const hasName = (arr, name) => {
   return arr.some(element => element === name);
 }
 
-console.log(hasName(names, 'Bruna'))
+// console.log(hasName(names, 'Bruna'))
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every(element => element.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 16));
