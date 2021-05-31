4582bce09ee503890f7d6d47ad3530c4eea363a0
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-31 11:30:17 -0300

    day 8.5 - content example 4.3

--- bloco_8/dia_5/content/script4.js
+++ bloco_8/dia_5/content/script4.js
@@ -10,6 +10,14 @@ let bebida = "arroz";
 
 [comida, animal, bebida] = [bebida, comida, animal];
 
-console.log(comida, animal, bebida); // arroz gato água
+// console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,,...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
