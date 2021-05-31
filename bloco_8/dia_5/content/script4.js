a94ceada393dc1bb10bbd9090e14050ada9c89fa
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-31 10:53:11 -0300

    day 8.5 - content example 4.2

--- bloco_8/dia_5/content/script4.js
+++ bloco_8/dia_5/content/script4.js
@@ -2,4 +2,14 @@ const saudacoes = ["Olá, tudo bem?", (saudacao) => console.log(saudacao)];
 
 const [msg, printMsg] = saudacoes;
 
-printMsg(msg);
+// printMsg(msg);
+
+let comida = "gato";
+let animal = "água";
+let bebida = "arroz";
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
