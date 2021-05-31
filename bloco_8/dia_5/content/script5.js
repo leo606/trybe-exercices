395d866c25bcf5198175d0d3807c8ad86bd7ad6c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-31 12:36:22 -0300

    day 8.5 - content example 5

---
+++ bloco_8/dia_5/content/script5.js
@@ -0,0 +1,25 @@
+// const person = {
+//   name: "João",
+//   lastName: "Jr",
+//   age: 34,
+// };
+
+// const { nationality='brazilian' } = person;
+
+// console.log(person);
+
+const getNationality = ({ firstName, nationality='Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
