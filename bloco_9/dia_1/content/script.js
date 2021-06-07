eccfacf483d8aa4d64ca738e8026b81938eed1a1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-06 23:06:51 -0300

    day 9.1 - content 1

---
+++ bloco_9/dia_1/content/script.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  console.log('comprar parafusos');
+  console.log('Adicionar ao estoque');
+}, 1);
+
+console.log('1 - receber roda');
+console.log('2 - encaixar parafuso');
+console.log('3 - fixar roda no carro');
