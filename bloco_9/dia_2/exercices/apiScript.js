da099e33cdd0d5b68132c02586aec1227bdb2663
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-09 10:24:57 -0300

    day 9.2 - exercise 2

--- bloco_9/dia_2/exercices/apiScript.js
+++ bloco_9/dia_2/exercices/apiScript.js
@@ -2,17 +2,32 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  const myObject = {
-    method: 'GET',
-    headers: { Accept: 'application/json' },
-  };
-  fetch(API_URL, myObject).then((response) => response.json())
-  .then((elem)=> appendJoke(elem.joke));
+  return new Promise((resolve, reject) => {
+    const myObject = {
+      method: 'GET',
+      headers: { Accept: 'application/json' },
+    };
+
+    const numbersArray = [];
+    for (let index = 0; index < 10; index += 1) {
+      numbersArray.push(Math.ceil(Math.random() * 50) ** 2);
+    }
+    const soma = numbersArray.reduce((acc, curr) => acc + curr);
+    if (soma < 8000) {
+      resolve([soma / 2, soma / 3, soma / 5, soma / 10]);
+    } else {
+      reject();
+    }
+  });
 };
 
-function appendJoke(joke){
+function appendJoke(joke) {
   const jokeContainer = document.getElementById('jokeContainer');
   jokeContainer.innerHTML = joke;
 }
 
-window.onload = () => fetchJoke();
+window.onload = () =>
+  fetchJoke()
+    .then((soma) => {console.log(soma); return soma})
+    .then((soma)=> console.log(soma))
+    .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
