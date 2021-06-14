8bedcd2e5d43cd874b8f2126d551f0b4626086cb
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-14 17:16:01 -0300

    day 10.1 - exercise - part 2 - 1.4, 1.5

--- bloco_10/day_1/exercises/tests/challenges.test.js
+++ bloco_10/day_1/exercises/tests/challenges.test.js
@@ -10,7 +10,19 @@ describe('>>> função encode', () => {
   });
 
   it('vogais são convertidas', () => {
-    expect(challenges.encode('a e i o u')).toEqual('1 2 3 4 5');
+    expect(challenges.encode('a e i o u')).toBe('1 2 3 4 5');
+  });
+
+  test('consoantes não são convertidas', () => {
+    expect(challenges.encode('b c d f g h j k l m n p q r s t v w x y z')).toBe(
+      'b c d f g h j k l m n p q r s t v w x y z'
+    );
+  });
+
+  test('número de caracteres não ser alterado', () => {
+    const input = 'aeiou';
+    const response = challenges.encode(input);
+    expect(response.length).toBe(5);
   });
 });
 
@@ -25,5 +37,9 @@ describe('>>> função decode', () => {
 
   it('vogais são convertidas', () => {
     expect(challenges.decode('1 2 3 4 5')).toEqual('a e i o u');
+  });
+
+  test('outros números não são convertidos', () => {
+    expect(challenges.decode('6 7 8 9 0')).toBe('6 7 8 9 0');
   });
 });
