2bb4b7ec9c3305b6c837b6bf7f70179c8906d710
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-14 15:36:12 -0300

    day 10.1 - exercise  1.2

--- bloco_10/day_1/exercises/tests/sum.test.js
+++ bloco_10/day_1/exercises/tests/sum.test.js
@@ -5,4 +5,8 @@ describe('>>> Retorno da função sum', () => {
   it('4 mais 5 ser 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
+
+  it('0 mais 0 ser 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
 });
