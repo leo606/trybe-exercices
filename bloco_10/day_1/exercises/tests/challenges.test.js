f26ca1385b1b1aaac79784cade45aa0756671289
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-14 16:42:30 -0300

    day 10.1 - exercise - part 2 - 1.3

--- bloco_10/day_1/exercises/tests/challenges.test.js
+++ bloco_10/day_1/exercises/tests/challenges.test.js
@@ -1,15 +1,29 @@
 const challenges = require('../challenges');
 
-describe('funcao encode', () => {
+describe('>>> função encode', () => {
   it('encode estar definida', () => {
     expect(challenges.encode).toBeDefined();
   });
 
-  test('encode ser funcao', () => {
-    expect(typeof challenges.encode).toEqual('function');
+  test('encode ser função', () => {
+    expect(typeof challenges.encode).toBe('function');
   });
 
   it('vogais são convertidas', () => {
-    expect(challenges.encode('a e i o u')).toBe('1 2 3 4 5');
+    expect(challenges.encode('a e i o u')).toEqual('1 2 3 4 5');
+  });
+});
+
+describe('>>> função decode', () => {
+  it('decode estar definida', () => {
+    expect(challenges.decode).toBeDefined();
+  });
+
+  test('decode ser função', () => {
+    expect(typeof challenges.decode).toBe('function');
+  });
+
+  it('vogais são convertidas', () => {
+    expect(challenges.decode('1 2 3 4 5')).toEqual('a e i o u');
   });
 });
