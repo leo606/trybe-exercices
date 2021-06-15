a8aa57bf825c3c97d87ac65d70f5ddac62fab562
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-14 23:17:24 -0300

    day 10.1 - bonus - toBeDefined

---
+++ bloco_10/day_1/exercises/tests/professional_board.test.js
@@ -0,0 +1,9 @@
+const searchEmployee = require('../professional_board');
+
+describe('searchEmployee test', () => {
+  it('searchEmployee is defined', () => {
+    expect(searchEmployee).toBeDefined();
+  });
+
+  it()
+});
