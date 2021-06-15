4d08537371bdddbecc9bfca3c3bc130eb56ddbe5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-15 12:36:59 -0300

    day 10.2 - content 1.2

---
+++ bloco_10/day_2/content/script.js
@@ -0,0 +1,8 @@
+const sumNum = (a, b, callback) => {
+  setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  }, 500);
+};
+
+module.exports = sumNum;
--- bloco_10/day_2/content/tests/script.test.js
+++ bloco_10/day_2/content/tests/script.test.js
@@ -1,7 +1,18 @@
+const sumNum = require('../script');
+
 test('não deve passar', (done) => {
   setTimeout(() => {
-    expect(10).toBe(10);
+    expect(10).toBe(5);
     console.log('deveria falhar');
     done();
   }, 2000);
+});
+
+describe('função sumNum', () => {
+  it('teste de soma 5 mais 10', (done) => {
+    sumNum(5, 10, (result) => {
+      expect(result).toBe(15);
+      done();
+    });
+  });
 });
