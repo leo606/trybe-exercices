9b8a67fa741d0401d34d3e5bbb2edc56687c339a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-16 15:48:14 -0300

    day 10.3 - content 3

--- bloco_10/day_3/content/tests/math.test.js
+++ bloco_10/day_3/content/tests/math.test.js
@@ -1,12 +1,13 @@
 const { test, expect } = require('@jest/globals');
 const math = require('../math');
 
-jest.mock('../math');
+// jest.mock('../math');
 
 test('somar', () => {
-  math.somar.mockImplementation((a, b) => a + b);
-  math.somar(1, 2);
-  expect(math.somar).toHaveBeenCalledTimes(1);
-  expect(math.somar).toHaveBeenLastCalledWith(1, 2);
-  expect(math.somar(1, 2)).toBe(3);
+  const spySomar = jest.spyOn(math, 'somar');
+
+  spySomar(1, 2);
+  expect(spySomar).toHaveBeenCalledTimes(1);
+  expect(spySomar).toHaveBeenCalledWith(1, 2);
+  expect(spySomar(1, 2)).resolves.toBe(3);
 });
