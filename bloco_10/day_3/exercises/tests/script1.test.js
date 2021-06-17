525c895b4f2f550ec29bfc9eef022dfbd7f29789
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-17 10:58:01 -0300

    day 10.3 - exercise 3

--- bloco_10/day_3/exercises/tests/script1.test.js
+++ bloco_10/day_3/exercises/tests/script1.test.js
@@ -2,7 +2,6 @@ const script1 = require('../script1');
 // jest.mock('../script1');
 
 describe('testes da funcao random', () => {
-  
   test('called', () => {
     script1.randomNumber = jest.fn().mockReturnValue(33);
     script1.randomNumber();
@@ -12,10 +11,16 @@ describe('testes da funcao random', () => {
   });
 
   test('retorno', () => {
-    script1.randomNumber = jest.fn().mockReturnValue(33);
     expect(script1.randomNumber()).toBe(33);
   });
 });
+
+// describe('testar retorno original', () => {
+//   test('original', () => {
+//     // script1.randomNumber.mockRestore();
+//     expect(script1.randomNumber()).toBe(44);
+//   });
+// });
 
 describe('implementation ', () => {
   test('funcao dividir', () => {
@@ -24,9 +29,18 @@ describe('implementation ', () => {
   });
 });
 
-// describe('testar retorno original', () => {
-//   test('original', () => {
-//     script1.randomNumber.mockReset();
-//     expect(script1.randomNumber()).toBe(44);
-//   });
-// });
+describe('receba três parâmetros e retorne sua multiplicação', () => {
+  test('multiplicacao', () => {
+    script1.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+    expect(script1.randomNumber(2, 2, 2)).toBe(8);
+    expect(script1.randomNumber).toHaveBeenCalledTimes(1);
+    expect(script1.randomNumber).toHaveBeenCalledWith(2, 2, 2);
+
+    script1.randomNumber.mockReset();
+    script1.randomNumber = jest.fn().mockImplementation((a) => a * 2);
+    expect(script1.randomNumber(3)).toBe(6);
+    expect(script1.randomNumber).toHaveBeenCalledTimes(1);
+    expect(script1.randomNumber).toHaveBeenCalledWith(3);
+
+  });
+});
