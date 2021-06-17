6ef3e2e87a152d15775e3870a0959c4003b1b1ec
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-17 12:38:53 -0300

    day 10.3 - exercise 4 - correcting

--- bloco_10/day_3/exercises/tests/script2.test.js
+++ bloco_10/day_3/exercises/tests/script2.test.js
@@ -1,32 +1,43 @@
-const { upper, firstLetter, concatStrings } = require('../script2');
+const script = require('../script2');
 
 jest.mock('../script2');
 
 describe('nova implementação para upper', () => {
   test('upper lower', () => {
-    upper.mockImplementation((str) => str.toLowerCase());
-    expect(upper('TEST')).toBe('test');
-    expect(upper).toHaveBeenCalled();
-    expect(upper).toHaveBeenCalledWith('TEST');
-    expect(upper).toHaveBeenCalledTimes(1);
+    // upper.mockImplementation((str) => str.toLowerCase());
+
+    const mockUpper = jest
+      .spyOn(script, 'upper')
+      .mockImplementation((str) => str.toLowerCase());
+
+    expect(mockUpper('TEST')).toBe('test');
+    expect(mockUpper).toHaveBeenCalled();
+    expect(mockUpper).toHaveBeenCalledWith('TEST');
+    expect(mockUpper).toHaveBeenCalledTimes(1);
   });
 });
 
 describe('nova implementação para firstLetter', () => {
   test('firstLetter lastLetter', () => {
-    firstLetter.mockImplementation((str) => str[str.length - 1]);
-    expect(firstLetter('xablau')).toBe('u');
-    expect(firstLetter('giraffe')).toBe('e');
-    expect(firstLetter).toHaveBeenCalledTimes(2);
-    expect(firstLetter).toHaveBeenCalledWith('xablau');
+    const firstLetterMock = jest
+      .spyOn(script, 'firstLetter')
+      .mockImplementation((str) => str[str.length - 1]);
+
+    expect(firstLetterMock('xablau')).toBe('u');
+    expect(firstLetterMock('giraffe')).toBe('e');
+    expect(firstLetterMock).toHaveBeenCalledTimes(2);
+    expect(firstLetterMock).toHaveBeenCalledWith('xablau');
   });
 });
 
 describe('nova implementação para concatStrings', () => {
   test('firstLetter concatStrings', () => {
-    concatStrings.mockImplementation((str1, str2, str3) => str1 + str2 + str3);
-    expect(concatStrings('um', 'dois', 'tres')).toBe('umdoistres');
-    expect(concatStrings).toHaveBeenCalled();
-    expect(concatStrings).toHaveBeenCalledWith('um', 'dois', 'tres');
+    const concatStringsMock = jest
+      .spyOn(script, 'concatStrings')
+      .mockImplementation((str1, str2, str3) => str1 + str2 + str3);
+
+    expect(concatStringsMock('um', 'dois', 'tres')).toBe('umdoistres');
+    expect(concatStringsMock).toHaveBeenCalled();
+    expect(concatStringsMock).toHaveBeenCalledWith('um', 'dois', 'tres');
   });
 });
