9b2a271c339be5082e3564ed69030f66e8d08cdd
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-18 16:32:55 -0300

    day 11.1 - exercises 2 - calling Task in App

--- bloco_11/day_1/exercises/ex11.1/src/App.js
+++ bloco_11/day_1/exercises/ex11.1/src/App.js
@@ -6,24 +6,7 @@ const Task = (value) => {
 };
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return Task('Hey there!');
 }
 
 export default App;
