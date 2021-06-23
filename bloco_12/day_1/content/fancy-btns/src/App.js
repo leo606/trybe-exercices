d48b9ca3b4786f6e93f7091e6836a0bbbd7cea21
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 13:35:59 -0300

    day 12.1 - content - onclick this

--- bloco_12/day_1/content/fancy-btns/src/App.js
+++ bloco_12/day_1/content/fancy-btns/src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
 import React from 'react';
 
-function handleClick1() {
-  console.log('click1');
-}
-
-function handleClick2() {
-  console.log('click2');
-}
-
-function handleClick3() {
-  console.log('click3');
-}
-
 class App extends React.Component {
+  handleClick() {
+    // Essa chamada ao `this` retorna `undefined`? !
+    console.log(this)
+    console.log('Clicou')
+  }
+
   render() {
     return (
-      <div>
-        <button onClick={handleClick1}>BUTTON1</button>
-        <button onClick={handleClick2}>BUTTON2</button>
-        <button onClick={handleClick3}>BUTTON3</button>
-      </div>
+      <button onClick={this.handleClick}>Hello World!</button>
     );
   }
 }
