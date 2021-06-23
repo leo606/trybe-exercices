1f50a67350ce491f9204dac2446ef6f691d3d394
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-23 10:13:16 -0300

    day 12.1 - content - btn click handler 3 btns

--- bloco_12/content/fancy-btns/src/App.js
+++ bloco_12/content/fancy-btns/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import React from 'react';
 
-function handleClick() {
-  console.log('click');
+function handleClick1() {
+  console.log('click1');
+}
+
+function handleClick2() {
+  console.log('click2');
+}
+
+function handleClick3() {
+  console.log('click3');
 }
 
 class App extends React.Component {
   render() {
-    return <button onClick={handleClick}>BUTTON</button>;
+    return (
+      <div>
+        <button onClick={handleClick1}>BUTTON1</button>
+        <button onClick={handleClick2}>BUTTON2</button>
+        <button onClick={handleClick3}>BUTTON3</button>
+      </div>
+    );
   }
 }
 
