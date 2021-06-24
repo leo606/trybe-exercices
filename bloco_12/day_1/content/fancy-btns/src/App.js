dc549a021bb7abebebe23faff9af88087f6fcfd5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-24 10:16:39 -0300

    day 12.1 - content - refresh clickCount using three buttons

--- bloco_12/day_1/content/fancy-btns/src/App.js
+++ bloco_12/day_1/content/fancy-btns/src/App.js
@@ -5,17 +5,35 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      clickCount: 0,
+      clickCount1: 0,
+      clickCount2: 0,
+      clickCount3: 0,
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
   }
 
-  handleClick() {
-    this.setState((curr, _props) => ({ clickCount: curr.clickCount + 1 }));
+  handleClick1() {
+    this.setState((curr, _props) => ({ clickCount1: curr.clickCount1 + 1 }));
+  }
+
+  handleClick2() {
+    this.setState((curr, _props) => ({ clickCount2: curr.clickCount2 + 1 }));
+  }
+
+  handleClick3() {
+    this.setState((curr, _props) => ({ clickCount3: curr.clickCount3 + 1 }));
   }
 
   render() {
-    return <button onClick={this.handleClick}>{this.state.clickCount}</button>;
+    return (
+      <div>
+        <button onClick={this.handleClick1}>{this.state.clickCount1}</button>
+        <button onClick={this.handleClick2}>{this.state.clickCount2}</button>
+        <button onClick={this.handleClick3}>{this.state.clickCount3}</button>
+      </div>
+    );
   }
 }
 
