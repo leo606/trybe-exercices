8166d452fafd102415ae7c1411ea650464b56506
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-24 13:45:16 -0300

    day 12.1 - content - changing btns colors

--- bloco_12/day_1/content/fancy-btns/src/App.js
+++ bloco_12/day_1/content/fancy-btns/src/App.js
@@ -14,8 +14,15 @@ class App extends React.Component {
     this.handleClick3 = this.handleClick3.bind(this);
   }
 
-  handleClick1() {
+  handleClick1(param) {
     this.setState((curr, _props) => ({ clickCount1: curr.clickCount1 + 1 }));
+    console.log(param.target.style.backgroundColor);
+
+    if (this.state.clickCount1 % 2 !== 0) {
+      param.target.style.backgroundColor = 'green';
+    } else {
+      param.target.style.backgroundColor = '';
+    }
   }
 
   handleClick2() {
@@ -29,7 +36,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.handleClick1}>{this.state.clickCount1}</button>
+        <button onClick={(param) => this.handleClick1(param)}>
+          {this.state.clickCount1}
+        </button>
         <button onClick={this.handleClick2}>{this.state.clickCount2}</button>
         <button onClick={this.handleClick3}>{this.state.clickCount3}</button>
       </div>
