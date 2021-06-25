16c149951f10fc1a00433a52ae60ee34a2f48307
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-25 10:19:25 -0300

    add btns for prec poke

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -7,7 +7,7 @@ class Pokedex extends React.Component {
     super();
     this.state = {
       pokemonIndex: 0,
-      pokeType: 'Fire',
+      pokeType: 'Electric',
     };
     this.handleClick = this.handleClick.bind(this);
     this.pokeTypeHandle = this.pokeTypeHandle.bind(this);
@@ -19,25 +19,42 @@ class Pokedex extends React.Component {
     return pokemons.filter((poke) => poke.type === type);
   }
 
-  handleClick(arrayLength) {
-    if (this.state.pokemonIndex < arrayLength - 1) {
+  handleClick(arrayLength, prev) {
+    const { pokemonIndex } = this.state;
+    console.log(pokemonIndex);
+    if (prev && pokemonIndex > 0) {
       this.setState((currState, _props) => ({
-        pokemonIndex: currState.pokemonIndex + 1,
+        pokemonIndex: currState.pokemonIndex - 1,
+      }));
+    } else if (pokemonIndex < arrayLength - 1) {
+      this.setState((currState, _props) => ({
+        pokemonIndex: prev
+          ? currState.pokemonIndex - 1
+          : currState.pokemonIndex + 1,
       }));
     }
   }
 
   render() {
     const pokesOfType = this.pokeTypeHandle(this.state.pokeType);
-    const pokesTypes = this.props.pokemons.map((elem) => elem.type);
+    const pokesTypes = this.props.pokemons
+      .map((elem) => elem.type)
+      .filter((elem, index, array) => index === array.indexOf(elem)); // source: https://stackoverflow.com/questions/16747798/delete-duplicate-elements-from-an-array
 
     return (
       <div>
         <Pokemon pokemon={pokesOfType[this.state.pokemonIndex]} />
         {pokesTypes.map((elem) => (
-          <Button clicLis={() => this.pokeTypeHandle(elem)} btnText={elem} />
+          <Button
+            key={elem}
+            clicLis={() => this.pokeTypeHandle(elem)}
+            btnText={elem}
+          />
         ))}
         <br />
+        <button onClick={(event) => this.handleClick(pokesOfType.length, true)}>
+          Previous Pokemon
+        </button>
 
         <button onClick={(event) => this.handleClick(pokesOfType.length)}>
           Next Pokemon
