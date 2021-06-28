5c6d33fadfb5609184f2f4dc2a621d3aa5ddf701
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-06-28 13:10:19 -0300

    day 13.1 - content - rickandmorty fetch API

--- bloco_13/day_1/content/interdimensional-app/src/App.js
+++ bloco_13/day_1/content/interdimensional-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+
+    this.fetchCharacter = this.fetchCharacter.bind(this);
+  }
+
+  async fetchCharacter() {
+    try {
+      const fetchRequest = await fetch(
+        'https://rickandmortyapi.com/api/character'
+      );
+      const requestObj = await fetchRequest.json();
+      this.setState({ characters: requestObj.results });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchCharacter();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Rick and Morty Characters: </h1>
+        <section className="body">
+          {this.state.characters.map((elem) => (
+            <div className='container' key={elem.name}>
+              <h3>{elem.name}</h3>
+              <img src={elem.image} alt={elem.name} />
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
