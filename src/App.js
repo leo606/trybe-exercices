db3aa923a37eb74484143ded667317f5f539eb89
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-01 13:21:00 -0300

    loadSavedDog

--- src/App.js
+++ src/App.js
@@ -5,53 +5,69 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      dog: undefined,
+      dog: '',
       loading: true,
     };
     this.fetchDog = this.fetchDog.bind(this);
     this.renderImgElement = this.renderImgElement.bind(this);
+    this.saveToLocalStorage = this.saveToLocalStorage.bind(this);
+    this.loadSavedDog = this.loadSavedDog.bind(this);
   }
 
   componentDidMount() {
+    const { dog } = localStorage;
+    if (dog) return this.loadSavedDog(dog);
     this.fetchDog();
   }
 
+  shouldComponentUpdate(nextProps, { dog }) {
+    console.log('dog - ', dog);
+    return !dog.includes('terrier');
+  }
+
+  componentDidUpdate() {
+    this.saveToLocalStorage();
+  }
+
+  loadSavedDog(dog) {
+    this.setState({ dog, loading: false });
+  }
+
+  saveToLocalStorage() {
+    const { dog } = this.state;
+    localStorage.dog = dog;
+  }
+
   async fetchDog() {
-    this.setState({ loading: true }, async () => {
-      try {
-        const headers = { headers: { Accept: 'application/json' } };
-        const fetchResponse = await fetch(
-          'https://dog.ceo/api/breeds/image/random',
-          headers,
-        );
-        const dogObj = await fetchResponse.json();
-        if (dogObj.message.includes('terrier')) {
-          this.setState({ loading: false });
-          return;
-        }
-        this.setState({ dog: dogObj });
-        this.setState({ loading: false });
-      } catch (err) {
-        console.log(err);
-      }
-    });
+    console.log('fetchDog');
+    const DOG_URL = 'https://dog.ceo/api/breeds/image/random';
+    const fetchRequest = await fetch(DOG_URL);
+    const dogObj = await fetchRequest.json();
+    this.setState({ dog: dogObj.message, loading: false });
   }
 
   renderImgElement() {
     const { dog } = this.state;
     return (
       <div>
-        <img src={ dog.message } alt="dog" />
-        <button type="button" onClick={ this.fetchDog }>
-          DOG!!
-        </button>
+        <img src={ dog } alt="dog" />
       </div>
     );
   }
 
   render() {
     const { loading } = this.state;
-    return <div>{loading ? 'Loading...' : this.renderImgElement()}</div>;
+
+    return (
+      <div>
+        <div>
+          <button type="button" onClick={ this.fetchDog }>
+            DOG!!
+          </button>
+        </div>
+        {loading ? 'Loading...' : this.renderImgElement()}
+      </div>
+    );
   }
 }
 
