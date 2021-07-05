547bb1f0b8349f271a9e1649cded147eae89b910
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-05 18:18:25 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Header from './Header';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <Route path='/' component={ Header } />
       <Route path='/' render={(props)=> <Pokedex pokemons={pokemons} />} />
     </BrowserRouter>
   );
