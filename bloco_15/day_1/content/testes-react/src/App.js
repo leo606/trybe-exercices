583e527a2e0a505393de532e332a4f456e6a4d07
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-16 15:05:40 -0300

    show email e describe

--- bloco_15/day_1/content/testes-react/src/App.js
+++ bloco_15/day_1/content/testes-react/src/App.js
@@ -1,15 +1,44 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <label htmlFor="id-email">
-        Email
-      </label>
-      <input type="email" id='id-email'/>
-      <input id='btn-send' type="button" value='Enviar' />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+  textHandler(value) {
+    this.setState({email: value});
+  }
+
+  changeSaveEmail(email) {
+    this.setState({ saveEmail: email, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">Email</label>
+        <input
+          type="email"
+          id="id-email"
+          onChange={(e) => this.textHandler(e.target.value)}
+        />
+        <input
+          data-testid="id-send"
+          id="btn-send"
+          type="button"
+          value="Enviar"
+          onClick={() => this.changeSaveEmail(email)}
+        />
+        <input id="btn-back" type="button" value="Voltar" />
+        <h2 data-testid="id-email-user">{`Email: ${saveEmail}`}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
--- bloco_15/day_1/content/testes-react/src/App.test.js
+++ bloco_15/day_1/content/testes-react/src/App.test.js
@@ -1,15 +1,27 @@
-import { render } from '@testing-library/react';
+import { getByTestId, render } from '@testing-library/react';
 import App from './App';
 
-test('verifica input email', () => {
-  const { getByLabelText } = render(<App />);
-  const inputEmail = getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail.type).toBe('email');
-});
+describe('TELA DE INSERÇÃO DE EMAIL', () => {
+  it('verifica input email', () => {
+    const { getByLabelText } = render(<App />);
+    const inputEmail = getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail.type).toBe('email');
+  });
 
-test('verifica se existe um botao', () => {
-  const { getByRole } = render(<App />);
-  const btn = getByRole('button');
-  expect(btn).toBeInTheDocument();
+  it('verifica se existe um botao', () => {
+    const { getByTestId } = render(<App />);
+    const btn = getByTestId('id-send');
+    expect(btn).toBeInTheDocument();
+    expect(btn.value).toBe('Enviar');
+  });
+
+  it('verifica se existe dois botoes', () => {
+    const { getAllByRole } = render(<App />);
+    const btn = getAllByRole('button');
+    expect(btn.length).toBe(2);
+    // expect(btn.value).toBe('Enviar');
+  });
+
+  
 });
