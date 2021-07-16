3e4ef8e3ec24aeb815290b80540424bb8096af44
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-16 19:06:04 -0300

    ex 2.1

--- src/test/exercise-two.test.js
+++ src/test/exercise-two.test.js
@@ -8,8 +8,20 @@ afterEach(cleanup);
 describe('Teste do campo de input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
-    const {} = render(<App />) // Caso precise de uma nova query adicione no object destructuring
+    const { getByLabelText, getByRole, getAllByRole } = render(<App />) // Caso precise de uma nova query adicione no object destructuring
 
+    const input = getByLabelText('Tarefa:');
+    const btn = getByRole('button');
+
+    listTodo.forEach((task)=> {
+      fireEvent.change(input, { target: { value: task } });
+      fireEvent.click(btn);
+    });
+    
+    const lis = getAllByRole('listitem');
+    expect(lis.length).toBe(listTodo.length);
+    expect(lis[1]).toHaveTextContent(listTodo[1]);
+    
   })
 });
 
