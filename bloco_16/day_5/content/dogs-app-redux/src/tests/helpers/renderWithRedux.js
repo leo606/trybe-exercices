28fbb9c0bd0e109c1441f9e9cd22257b51a81d62
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-30 08:55:51 -0300

    add render with redux

---
+++ bloco_16/day_5/content/dogs-app-redux/src/tests/helpers/renderWithRedux.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { reducer } from '../../redux/reducers/reducer';
+
+const createMockStore = (initialState) =>
+  createStore(combineReducers({ reducer }), initialState, applyMiddleware(thunk));
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createMockStore(initialState) } = {}
+) => ({ ...render(<Provider store={store}>{component}</Provider>), store });
+
+export default renderWithRedux;
