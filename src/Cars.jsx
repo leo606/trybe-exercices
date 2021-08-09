51880cf93850987f9e478eb55c29e93f4bcd7050
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-08-09 13:21:04 -0300

    day 18.1 - exercise 1

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -2,55 +2,51 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
+import MyContext from './contextAPI/MyContext';
+// import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
-import { moveCar } from './redux/actionCreators';
+// import { moveCar } from './redux/actionCreators';
 
-function Cars({ redCar, blueCar, yellowCar, moveCar }) {
+function Cars() {
   return (
-    <div>
-      <div>
-        <img
-          className={redCar ? 'car-right' : 'car-left'}
-          src={carRed}
-          alt="red car"
-        />
-        <button
-          onClick={() => moveCar('red', !redCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={blueCar ? 'car-right' : 'car-left'}
-          src={carBlue}
-          alt="blue car"
-        />
-        <button
-          onClick={() => moveCar('blue', !blueCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={yellowCar ? 'car-right' : 'car-left'}
-          src={carYellow}
-          alt="yellow car"
-        />
-        <button
-          onClick={() => moveCar('yellow', !yellowCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-    </div>
+    <MyContext.Consumer>
+      {({ redCar, blueCar, yellowCar, moveCar }) => (
+        <>
+          <div>
+            <img
+              className={redCar ? 'car-right' : 'car-left'}
+              src={carRed}
+              alt="red car"
+            />
+            <button onClick={() => moveCar('redCar', !redCar)} type="button">
+              Move
+            </button>
+          </div>
+          <div>
+            <img
+              className={blueCar ? 'car-right' : 'car-left'}
+              src={carBlue}
+              alt="blue car"
+            />
+            <button onClick={() => moveCar('blueCar', !blueCar)} type="button">
+              Move
+            </button>
+          </div>
+          <div>
+            <img
+              className={yellowCar ? 'car-right' : 'car-left'}
+              src={carYellow}
+              alt="yellow car"
+            />
+            <button onClick={() => moveCar('yellowCar', !yellowCar)} type="button">
+              Move
+            </button>
+          </div>
+        </>
+      )}
+    </MyContext.Consumer>
   );
 }
 
@@ -61,11 +57,14 @@ Cars.propTypes = {
   yellowCar: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  redCar: state.cars.red,
-  blueCar: state.cars.blue,
-  yellowCar: state.cars.yellow});
+// const mapStateToProps = (state) => ({
+//   redCar: state.cars.red,
+//   blueCar: state.cars.blue,
+//   yellowCar: state.cars.yellow,
+// });
 
-const mapDispatchToProps = { moveCar };
+// const mapDispatchToProps = { moveCar };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+// export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+
+export default Cars;
