2624c9880a28b64a991624b88ef6bd442141aa4e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-09 18:38:10 -0300

    day 20.2 - ex 6

---
+++ bloco_20/day_2/exercises/ex6.sql
@@ -0,0 +1,6 @@
+-- Escreva uma query para exibir o nome como "Nome do Projeto" e as horas como "Tempo de Trabalho" de cada projeto.
+
+SELECT 
+    Name AS `Nome`, Hours AS `Horas de trabalho`
+FROM
+    Scientists.Projects;
