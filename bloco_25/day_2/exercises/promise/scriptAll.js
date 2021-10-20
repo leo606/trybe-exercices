d09d1b100ba5379b75df8147ce4c2e03a716daa5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-20 13:47:12 -0300

    use map inside Promisse.all

--- bloco_25/day_2/exercises/promise/scriptAll.js
+++ bloco_25/day_2/exercises/promise/scriptAll.js
@@ -3,12 +3,8 @@ const fs = require("fs").promises;
 function all() {
   const words = ["Finalmente", "estou", "usando", "Promise.all", "!!!"];
 
-  Promise.all([
-    fs.writeFile("./file1.txt", words[0]),
-    fs.writeFile("./file2.txt", words[1]),
-    fs.writeFile("./file3.txt", words[2]),
-    fs.writeFile("./file4.txt", words[3]),
-    fs.writeFile("./file5.txt", words[4]),
-  ]).then(() => console.log("files successfully created"));
+  Promise.all(
+    words.map((word, i) => fs.writeFile(`./file${i + 1}.txt`, word))
+  ).then(() => console.log("files successfully created"));
 }
 all();
