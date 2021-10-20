b7c7a02be814f3d4ed2aea03594b59b2c8339eb9
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-20 11:09:03 -0300

    day 25.2 - ex 4.4 - 4.5

--- bloco_25/day_2/exercises/promise/scriptSimpsons.js
+++ bloco_25/day_2/exercises/promise/scriptSimpsons.js
@@ -33,11 +33,46 @@ async function removeSimpsons() {
     .readFile("./simpsons.json", "utf-8")
     .then((data) => JSON.parse(data));
 
-  const charactersRemoved = characters.filter((c)=> c.id !== '10' && c.id !== '6');
+  const charactersRemoved = characters.filter(
+    (c) => c.id !== "10" && c.id !== "6"
+  );
 
   fs.writeFile("./simpsons.json", JSON.stringify(charactersRemoved, 2))
     .then(() => console.log("removido com sucesso"))
     .catch((e) => console.log(e.message));
 }
 
-removeSimpsons();
+async function simpsonFamily() {
+  const characters = await fs
+    .readFile("./simpsons.json", "utf-8")
+    .then((data) => JSON.parse(data))
+    .catch((e) => console.log(e.message));
+
+  const filteredData = characters.filter(
+    ({ id }) => id == 1 || id == 2 || id == 3 || id == 4
+  );
+
+  fs.writeFile("./simpsonFamily.json", JSON.stringify(filteredData))
+    .then(() => console.log("arquivo criado com sucesso"))
+    .catch((e) => console.log(e.message));
+}
+
+async function addNelson() {
+  const family = await fs
+    .readFile("./simpsonFamily.json", "utf-8")
+    .then((data) => JSON.parse(data))
+    .catch((e) => console.log(e.message));
+
+  const characters = await fs
+    .readFile("./simpsons.json", "utf-8")
+    .then((data) => JSON.parse(data))
+    .catch((e) => console.log(e.message));
+
+  const nelson = characters.findIndex((c)=> c.name === "Nelson Muntz")
+  family.push(characters[nelson]);
+
+  fs.writeFile("./simpsonFamily.json", JSON.stringify(family))
+    .then(() => console.log("arquivo criado com sucesso -- add nelson"))
+    .catch((e) => console.log(e.message));
+}
+addNelson();
--- bloco_25/day_2/exercises/promise/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-// idk(randomN(), randomN(), randomN())
-//   .then((msg)=> console.log(msg))
-//   .catch((e)=> console.log(e.message));
