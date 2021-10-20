1f38df9b0d139ad47746e69b87564c6f6c7bf5b3
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-20 08:14:02 -0300

    day 25.2 - ex 4.2

--- bloco_25/day_2/exercises/promise/scriptSimpsons.js
+++ bloco_25/day_2/exercises/promise/scriptSimpsons.js
@@ -12,4 +12,20 @@ async function printSimpsons() {
   }
 }
 
-printSimpsons();
+async function printSimpsonsById(inId) {
+  try {
+    const characters = await fs.readFile("./simpsons.json", "utf-8");
+    const charactersParse = JSON.parse(characters);
+    const chaIndex = charactersParse.findIndex(({ id }) => id == inId);
+    if (chaIndex >= 0) {
+      return console.log(
+        `${charactersParse[chaIndex].id} - ${charactersParse[chaIndex].name}`
+      );
+    }
+    throw new Error("id não encontrado");
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+printSimpsonsById(11);
