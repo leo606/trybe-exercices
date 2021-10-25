975182e2fe8f6c780e9e1535b0c5894875ed430a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-25 13:16:17 -0300

    day 25.4 - exercise 2

--- bloco_25/day_4/exercises/index.js
+++ bloco_25/day_4/exercises/index.js
@@ -8,4 +8,10 @@ app.route("/ping").get((req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
+app.route("/hello").post((req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ message: `Hello ${name}` });
+});
+
+
 app.listen(3001, () => console.log("listening on port 3001"));
