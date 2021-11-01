dd1bec9ad6059a3f1a209140c84dba70dfd83731
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-01 09:39:04 -0300

    change directory name

---
+++ bloco_26/day_1/content/model-example/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const Author = require("./models/Author");
+const Books = require("./models/Book");
+
+const app = express();
+app.use(express.json());
+
+app.get("/authors/:id", async (req, res) => {
+  const { id } = req.params;
+  const author = await Author.findByid(id);
+  if (!author) return res.status(404).json({ message: "not found" });
+  res.status(200).json(author);
+});
+
+app.get("/authors", async (req, res) => {
+  const authors = await Author.getAll();
+  res.status(200).json(authors);
+});
+
+app.post("/authors", async (req, res) => {
+  const obj = ({ first_name, middle_name, last_name } = req.body);
+  if (!Author.validAuthor(...Object.values(obj))) {
+    return res.status(400).json({ message: "invalid data" });
+  }
+  const saved = await Author.createAuthor(...Object.values(obj));
+  res.status(201).json({ message: "author saved", saved });
+});
+
+app.get("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const books = await Books.getByBookId(id);
+  if (!books) return res.status(404).json({ message: "not found" });
+  return res.status(200).json(books);
+});
+
+app.get("/books", async (req, res) => {
+  const { author_id } = req.query;
+  if (author_id) {
+    const books = await Books.getByAuthorId(+author_id);
+    if (!books) return res.status(404).json({ message: "not found" });
+    return res.status(200).json(books);
+  }
+  const books = await Books.getAll();
+  return res.status(200).json(books);
+});
+
+app.post("/books", async (req, res) => {
+  const obj = ({ title, author_id } = req.body);
+  // const isValid = await Books.validBook(...Object.values(obj));
+  // if (!isValid) {
+  //   return res.status(400).json({ message: "invalid data" });
+  // }
+  const saved = await Books.createBook(...Object.values(obj));
+
+  return res.status(201).json({ message: "book saved", saved });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
