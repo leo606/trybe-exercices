ba1c85def03b2a14388ae09abdd53f3e59f6ba6e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-01 10:34:25 -0300

    day 26.1 - exercises 2

--- bloco_26/day_1/exercises/routes/userRouter.js
+++ bloco_26/day_1/exercises/routes/userRouter.js
@@ -3,14 +3,20 @@ const userValidation = require("../validations/user");
 
 const router = express.Router();
 
-router.use(userValidation.checkFields);
+router.use(
+  userValidation.checkFields,
+  userValidation.checkPassword,
+  userValidation.checkFirstname,
+  userValidation.checkLastname,
+  userValidation.checkEmail
+);
 
 router.use((err, req, res, next) => {
-  res.status(err.status).json({ message: err.message });
+  res.status(err.status).json({ error: true, message: err.message });
 });
 
 router.post("/", (req, res, next) => {
-  res.status(200).send("ok");
+  res.status(200).json({ message: "ok" });
 });
 
 module.exports = router;
--- bloco_26/day_1/exercises/validations/user.js
+++ bloco_26/day_1/exercises/validations/user.js
@@ -1,10 +1,78 @@
+const emailRegex = /\S+@\S+\.\S+/;
+
 function checkFields(req, res, next) {
   const user = ({ firstName, lastName, email, password } = req.body);
 
   if (!user.firstName || !user.lastName || !user.email || !user.password) {
-    next({ status: 400, message: "fields error" });
+    next({
+      status: 400,
+      message:
+        "fields must be on format: 'firstName', 'lastName', 'email', 'password'",
+    });
   }
   next();
 }
 
-module.exports = { checkFields };
+function checkFirstname(req, res, next) {
+  const { firstName } = req.body;
+
+  if (typeof firstName !== "string") {
+    next({ status: 400, message: "firstName must be string" });
+  }
+
+  if (firstName.length < 3) {
+    next({ status: 400, message: "firstName must be 3 characters or more" });
+  }
+
+  next();
+}
+
+function checkLastname(req, res, next) {
+  const { lastName } = req.body;
+
+  if (typeof lastName !== "string") {
+    next({ status: 400, message: "lastName must be string" });
+  }
+
+  if (lastName.length < 3) {
+    next({ status: 400, message: "lastName must be 3 characters or more" });
+  }
+
+  next();
+}
+
+function checkEmail(req, res, next) {
+  const { email } = req.body;
+
+  if (typeof email !== "string") {
+    next({ status: 400, message: "email must be string" });
+  }
+
+  if (!emailRegex.test(email)) {
+    next({ status: 400, message: "email invalid" });
+  }
+
+  next();
+}
+
+function checkPassword(req, res, next) {
+  const { password } = req.body;
+
+  if (typeof password !== "string") {
+    next({ status: 400, message: "password must be string" });
+  }
+
+  if (password.length < 6) {
+    next({ status: 400, message: "password must be 6 characters or more" });
+  }
+
+  next();
+}
+
+module.exports = {
+  checkFields,
+  checkPassword,
+  checkFirstname,
+  checkLastname,
+  checkEmail,
+};
