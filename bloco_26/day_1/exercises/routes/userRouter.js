aafb3686992b48f515117e97bdfe23715bc6f5ca
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-02 16:20:00 -0300

    day 26.1 - exercises 4

--- bloco_26/day_1/exercises/routes/userRouter.js
+++ bloco_26/day_1/exercises/routes/userRouter.js
@@ -4,6 +4,30 @@ const user = require("../model/user");
 
 const router = express.Router();
 
+router.use((err, req, res, next) => {
+  res.status(err.status).json({ error: true, message: err.message });
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const userFound = await user.getUsersById(id);
+    if (!userFound) return res.status(404).json({ message: "not found" });
+    res.status(200).json(userFound);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await user.getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.use(
   userValidation.checkFields,
   userValidation.checkPassword,
@@ -11,10 +35,6 @@ router.use(
   userValidation.checkLastname,
   userValidation.checkEmail
 );
-
-router.use((err, req, res, next) => {
-  res.status(err.status).json({ error: true, message: err.message });
-});
 
 router.post("/", async (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
@@ -27,6 +47,22 @@ router.post("/", async (req, res, next) => {
     });
 
     res.status(201).json(inserted);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
+  const { id } = req.params;
+  try {
+    const update = await user.putUser(
+      { firstName, lastName, email, password },
+      id
+    );
+
+    if (!update) return res.status(402).json({ message: "not found" });
+    res.status(201).json(update);
   } catch (error) {
     console.log(error);
   }
