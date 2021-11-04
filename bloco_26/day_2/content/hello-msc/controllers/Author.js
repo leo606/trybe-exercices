62490688ba9199d21ed16f6adcf1e07adda93d52
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-04 13:17:01 -0300

    day 26.2 - content 2

--- bloco_26/day_2/content/hello-msc/controllers/Author.js
+++ bloco_26/day_2/content/hello-msc/controllers/Author.js
@@ -1,5 +1,6 @@
 const rescue = require("express-rescue");
 const service = require("../services/Author");
+const joi = require("joi");
 
 const getAll = rescue(async (req, res) => {
   const authors = await service.getAll();
@@ -7,14 +8,32 @@ const getAll = rescue(async (req, res) => {
   res.status(200).json(authors);
 });
 
-const findById = rescue((req, res) => {
+const findById = rescue((req, res, next) => {
   const { id } = req.params;
 
   const author = await service.findById(id);
 
-  if (author.error) return res.status(404).json(author.error);
+  if (author.error) return next(author.error);
 
   res.status(200).json(author);
+});
+
+const create = rescue(async (req, res, next) => {
+  const { error } = joi
+    .object({
+      firstName: joi.string().not().empty().required(),
+      middleName: joi.string().not().empty(),
+      lastName: joi.string().not().empty().required(),
+    })
+    .validate(req.boby);
+
+  if (error) return next(error);
+  const { firstName, middleName, lastName } = req.body;
+  const newAuthor = await service.create(firstName, middleName, lastName);
+
+  if (newAuthor.error) return next(newAuthor.error);
+
+  return res.status(201).json({ newAuthor });
 });
 
 module.exports = { getAll, findById };
