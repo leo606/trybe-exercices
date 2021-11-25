be1f497c40b33dba723746c1ff4a8544e491a5d5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-25 18:11:30 -0300

    day 27.2 - content 4

--- bloco_27/day_2/content/send-files/send.js
+++ bloco_27/day_2/content/send-files/send.js
@@ -1,14 +1,14 @@
-const FormData = require('form-data');
-const axios = require('axios');
-const fs = require('fs');
+const FormData = require("form-data");
+const axios = require("axios");
+const fs = require("fs");
 
 /* Criamos um stream de um arquivo */
-const stream = fs.createReadStream('./meu-arquivo.txt');
+const stream = fs.createReadStream(__dirname + "/my-file.txt");
 
 /* Aqui, criamos um formulário com um campo chamado 'file' que carregará */
 /* o stream do nosso arquivo */
 const form = new FormData();
-form.append('file', stream);
+form.append("file", stream);
 
 /* Esse arquivo não será enviado no body da requisição como de costume. */
 /* Em ambientes NodeJS, é preciso setar o valor de boundary no header */
@@ -16,7 +16,7 @@ form.append('file', stream);
 const formHeaders = form.getHeaders();
 
 axios
-  .post('http://localhost:3000/files/upload', form, {
+  .post("http://localhost:3000/uploads", form, {
     headers: {
       ...formHeaders,
     },
