5068a7c8da18d0716d668a8c1d6d7a54e54286ee
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-25 18:29:59 -0300

    day 27.2 - exercise 1

--- bloco_27/day_2/exercises/multer-exercises/index.js
+++ bloco_27/day_2/exercises/multer-exercises/index.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const { PORT } = process.env;
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => cb(null, `${Date.now()} - ${file.originalname}`),
+});
+
 const controllers = require('./controllers');
 const middlewares = require('./middlewares');
+
+const upload = multer({ storage });
 
 const app = express();
 
@@ -22,6 +30,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.status(200).json({ file: req.file.originalname });
+});
 
 app.use(middlewares.error);
 
