7424143800832eaaf98a3fff67bd32896a6798a5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-12-04 11:20:38 -0300

    day 28.1 - exercises - heroku

--- bloco_28/day_1/exercises/heroku-deploy/index.js
+++ bloco_28/day_1/exercises/heroku-deploy/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.send("running...");
+  res.send(process.env.MESSAGE || 'no message');
 });
 
 app.listen(PORT, () => console.log("listening on port", PORT));
