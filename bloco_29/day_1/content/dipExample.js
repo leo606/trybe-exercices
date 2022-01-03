914c8a73d224bd88f4b5f189df26179ceb987c99
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-03 08:41:59 -0300

    day 29.1 - content 5

--- bloco_29/day_1/content/dipExample.js
+++ bloco_29/day_1/content/dipExample.js
@@ -11,8 +11,16 @@ const requestWithAxios = () => {
     .then((response) => console.log(response.data));
 };
 
-// const requestWithFetch = () => {
-// ...
+const requestWithFetch = () => {
+  fetch(url, {
+    headers: new fetch.Headers({
+      Accept: 'application/json',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data.joke))
+    .catch((err) => console.log(err));
+};
 
 const getJokes = (numberOfJokes, jokeRequester = requestWithFetch) => {
   for (let i = 0; i < numberOfJokes; i += 1) jokeRequester();
