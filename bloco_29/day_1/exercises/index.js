e52ddddfe63369d433f25e3ebc37960a850d349f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-04 10:31:14 -0300

    day 29.1 - exercise 3

--- bloco_29/day_1/exercises/index.js
+++ bloco_29/day_1/exercises/index.js
@@ -7,19 +7,20 @@ app.use(express.json());
 
 app
   .route("/plant/:id")
-  .get((res, res) => {
+  .get((req, res) => {
     const { id } = req.params;
     const plant = plantsModule.getPlantById(id);
     res.status(200).json(plant);
   })
-  .put((res, res) => {
+  .post((req, res) => {
     const { id } = req.params;
     const { plant } = req.body;
     plantsModule.editPlant(id, plant);
     res.status(201).end();
   })
-  .delete((res, res) => {
+  .delete((req, res) => {
     const { id } = req.params;
+    plantsModule.removePlantById(id);
     res.status(200).end();
   });
 
@@ -33,5 +34,11 @@ app
     plantsModule.createNewPlant(plant);
     res.status(201).end();
   });
+
+app.route("sunny/:id").get((req, res) => {
+  const { id } = req.params;
+  const plants = plantsModule.getPlantsThatNeedsSunWithId(id);
+  res.status(200).json(plants);
+});
 
 app.listen(3000, () => console.log("listening", 3000));
