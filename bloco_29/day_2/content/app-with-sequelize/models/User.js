927896b379daae775db002b356e299e4bde2a033
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-04 15:24:03 -0300

    day 29.2 - content 2

--- bloco_29/day_2/content/app-with-sequelize/models/User.js
+++ bloco_29/day_2/content/app-with-sequelize/models/User.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
   });
   return User;
 };
