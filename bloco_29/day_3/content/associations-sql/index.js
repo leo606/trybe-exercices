375fe37f1d5162e328efe77104626e4a42f9fd95
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-05 15:30:43 -0300

    day 29.3 - content 8

--- bloco_29/day_3/content/associations-sql/index.js
+++ bloco_29/day_3/content/associations-sql/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { Address, Employee, Book, User } = require("./models");
+const Sequelize = require("sequelize");
+const config = require("./config/config");
 
 const app = express();
+
+app.use(express.json());
+
+const sequelize = new Sequelize(config.development);
 
 app.get("/employees", async (_req, res) => {
   try {
@@ -13,6 +19,30 @@ app.get("/employees", async (_req, res) => {
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: "Ocorreu um erro" });
+  }
+});
+
+app.post("/employees", async (req, res) => {
+  const trans = await sequelize.transaction();
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: trans }
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: trans }
+    );
+
+    await trans.commit();
+
+    return res.status(201).json({ message: "Cadastrado com sucesso" });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Algo deu errado" });
   }
 });
 
