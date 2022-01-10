cea63784756e88bb276f900288629a41fe5390cc
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-10 16:48:35 -0300

    day 30.1 - content 5 - urlencoded parser

--- bloco_30/day_1/content/mvc-example/index.js
+++ bloco_30/day_1/content/mvc-example/index.js
@@ -8,12 +8,12 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/authors", authorController.listAuthors);
 app.get("/authors/new", authorController.newAuthor);
 app.get("/authors/:id", authorController.showAuthor);
-app.post("author", authorController.createAuthor);
+app.post("/authors", authorController.createAuthor);
 
 const PORT = process.env.PORT || 3000;
 
