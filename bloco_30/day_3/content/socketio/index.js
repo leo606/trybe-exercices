7260e41b331ee1a6d63a03c582f6703a255dc586
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-13 10:35:01 -0300

    day 30.3 - content 2

--- bloco_30/day_3/content/socketio/index.js
+++ bloco_30/day_3/content/socketio/index.js
@@ -4,6 +4,17 @@ const app = express();
 
 const http = require("http").createServer(app);
 
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected. ID: ${socket.id}`);
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
