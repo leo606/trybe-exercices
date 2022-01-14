363f0a280abd8b4b576cd586676d48189fda1687
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-14 10:32:29 -0300

    server send msg

--- bloco_30/day_3/content/chat/public/js/chat.js
+++ bloco_30/day_3/content/chat/public/js/chat.js
@@ -6,6 +6,15 @@ const inputMessage = document.querySelector("#messageInput");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   socket.emit("clientMsg", inputMessage.value);
-  inputMessage.value = '';
+  inputMessage.value = "";
   return false;
 });
+
+function createMsg(msg) {
+  const messagesUl = document.getElementById("messages");
+  const li = document.createElement("li");
+  li.innerHTML = msg;
+  messagesUl.appendChild(li);
+}
+
+socket.on("serverMsg", createMsg);
--- bloco_30/day_3/content/chat/sockets/chat.js
+++ bloco_30/day_3/content/chat/sockets/chat.js
@@ -1,9 +1,8 @@
 module.exports = function (io) {
   io.on("connection", (socket) => {
-    console.log("conn");
-
     socket.on("clientMsg", (msg) => {
       console.log(`Message ${msg}`);
+      io.emit("serverMsg", msg);
     });
   });
 };
