8749426f47297a60cecb246bd079031fca856970
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-14 18:31:00 -0300

    broadcast

--- bloco_30/day_3/exercises/sockets/feed.js
+++ bloco_30/day_3/exercises/sockets/feed.js
@@ -5,12 +5,12 @@ module.exports = function (io) {
   io.on("connection", (socket) => {
     socket.on("likePost", () => {
       likeCount += 1;
-      socket.emit("updateLikes", likeCount);
+      socket.broadcast.emit("updateLikes", likeCount);
     });
 
     socket.on("starPost", () => {
       starCount += 1;
-      socket.emit("updateStars", starCount);
+      socket.broadcast.emit("updateStars", starCount);
     });
   });
 };
