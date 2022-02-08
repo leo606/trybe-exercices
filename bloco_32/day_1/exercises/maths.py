1474c18f88c1038296cdbd25b2fc2cd60a2a0f6b
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-08 13:03:18 -0300

    day 32.1 - exercise 3

--- bloco_32/day_1/exercises/maths.py
+++ bloco_32/day_1/exercises/maths.py
@@ -1,3 +1,4 @@
+# ercício 1: Crie uma função que receba dois números e retorne o maior deles.
 def higher_number(a, b):
     if a > b:
         return a
@@ -5,5 +6,12 @@ def higher_number(a, b):
         return b
 
 
+# Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
 def list_avg(list):
     return sum(list) / len(list)
+
+
+# Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+def print_square(n):
+    for i in range(0, n):
+        print('*' * n)
