b99dff407a22f0b1394bc4da270b5d908e08fe61
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-09 08:05:52 -0300

    day 32.2 - content 1

---
+++ bloco_32/day_2/content/inputs.py
@@ -0,0 +1,6 @@
+# Exercício 1: Faça um programa que solicite o nome de
+# uma pessoa usuária e imprima-o na vertical.
+def vertical_name():
+    name = input("what's your name")
+    for cha in name:
+        print(cha)
