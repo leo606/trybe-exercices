f2254b4089ee739dd102da572adc0bcebf585bf2
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-09 10:31:30 -0300

    day 32.2 - content pokes 2

--- bloco_32/day_2/content/json_manipulation.py
+++ bloco_32/day_2/content/json_manipulation.py
@@ -5,6 +5,5 @@ with open("./pokemons.json", mode="r") as poke_file:
 
 grass_pokes = [poke for poke in pokes if "Grass" in poke["type"]]
 
-with open("./grass_pokes.json", mode="w") as file:
-    json_type = json.dumps(grass_pokes)
-    file.write(json_type)
+with open("./grass_pokes2.json", mode="w") as file:
+    json.dump(grass_pokes, file)
