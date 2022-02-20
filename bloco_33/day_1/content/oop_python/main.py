7a1dbb33ceb6396828a0fcae6d8bd31194c67b9d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-20 19:43:48 -0300

    mailer class

--- bloco_33/day_1/content/oop_python/main.py
+++ bloco_33/day_1/content/oop_python/main.py
@@ -1,12 +1,36 @@
+import smtplib
+import ssl
+
+
 class User:
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
 
+    def reset_password(self):
+        print("send password reset email")
+
+
+class mailer:
+    def __init__(self, from_email, from_password, to_email):
+        self.from_email: from_email
+        self.from_password: from_password
+        self.to_email: to_email
+
+    def send_email(self, subject, message):
+        body = f"Subject:{subject}\n\n{message}".encode("utf-8")
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(
+            "smtp.gmail.com", 465, context=context
+        ) as server:
+            server.login(self.from_email, self.from_password)
+            try:
+                server.sendmail(self.from_email, self.to_email, body)
+            except (smtplib.SMTPRecipientsRefused, smtplib.SMTPSenderRefused):
+                raise ValueError
+
 
 some_user = User("fulano de tal", "fulano@detal.com", "fulano123")
 
-print(some_user.email)
-print(some_user.password)
-print(some_user.name)
+some_user.reset_password()
---
+++ bloco_33/day_1/content/oop_python/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+self.reset_password
