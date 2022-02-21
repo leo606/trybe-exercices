ff0c33babaaa9bb4737fd599da9d20f6e078ba9c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-21 18:30:42 -0300

    day 33.2 - content 2

--- bloco_33/day_2/content/sales_report/main.py
+++ bloco_33/day_2/content/sales_report/main.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 
 # Altere o código da classe SalesReport para que ela, além de gerar relatórios
@@ -8,8 +9,9 @@ import json
 
 
 class SalesReport:
-    def __init__(self, export_file):
-        self.export_file = export_file + ".json"
+    def __init__(self, export_file, type):
+        self.export_file = export_file
+        self.file_type = type
 
     def build(self):
         """Aqui colocamos a lógica para a entidade 'se criar',
@@ -22,5 +24,28 @@ class SalesReport:
 
     def serialize(self):
         # Vamos gerar, aqui, o nosso relatório em formato JSON
-        with open(self.export_file, "w") as file:
-            json.dump(self.build(), file)
+        with open(f"{self.export_file}.{self.file_type}", "w") as file:
+            if self.file_type == "json":
+                json.dump(self.build(), file)
+            elif self.file_type == "csv":
+                writer = csv.writer(file)
+                headers = [
+                    "Coluna 1",
+                    "Coluna 2",
+                    "Coluna 3",
+                ]
+                writer.writerow(headers)
+                for row in self.build():
+                    writer.writerow(row.values())
+
+
+# Classe, crie (em outras palavras, instancie!) uma nova entidade
+# 'Relatório de vendas' para eu usar!
+
+meu_relatorio_de_vendas_json = SalesReport("meu_relatorio", "json")
+meu_relatorio_de_vendas_csv = SalesReport("meu_relatorio", "csv")
+
+# Entidade 'meu_relatorio_de_vendas', que eu acabei de criar, imprima-se!
+
+meu_relatorio_de_vendas_json.serialize()
+meu_relatorio_de_vendas_csv.serialize()
