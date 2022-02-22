4d85f2faff54fb2b22d8e81d0a9e3b6f1ab196d9
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-22 17:56:43 -0300

    day 33.2 - exercises 1 - change channel

--- bloco_33/day_2/exercises/tv_oop/main.py
+++ bloco_33/day_2/exercises/tv_oop/main.py
@@ -2,4 +2,6 @@ from schemas.tv import Tv
 
 my_tv = Tv(3)
 
-my_tv.volume_down()
+print(my_tv.channel)
+my_tv.change_channel(20)
+print(my_tv.channel)
--- bloco_33/day_2/exercises/tv_oop/schemas/tv.py
+++ bloco_33/day_2/exercises/tv_oop/schemas/tv.py
@@ -2,7 +2,7 @@ class Tv:
     def __init__(self, size):
         self.volume = 50
         self.channel = 1
-        size = size
+        self._size = size
         self.on = False
 
     def volume_up(self):
@@ -12,3 +12,7 @@ class Tv:
     def volume_down(self):
         if self.volume > 0:
             self.volume -= 1
+
+    def change_channel(self, channel):
+        if 1 <= channel <= 99:
+            self.channel = channel
