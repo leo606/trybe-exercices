cf27ee5b82dabde53b39b06104812b732c7d4f5e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-22 19:00:00 -0300

    day 33.2 - exercises 2 - mode

--- bloco_33/day_2/exercises/statistics_oop/main.py
+++ bloco_33/day_2/exercises/statistics_oop/main.py
@@ -1,5 +1,7 @@
 from schemas.statistics import Statistics
 
-n_list = [1, 2, 3, 4]
+n_list = [99, 99, 1, 2, 7, 2, 99, 3]
 
-print('mean =>', Statistics.mean(n_list))
+print("mean =>", Statistics.mean(n_list))
+print("median =>", Statistics.median(n_list))
+print("modal =>", Statistics.modal(n_list))
--- bloco_33/day_2/exercises/statistics_oop/schemas/statistics.py
+++ bloco_33/day_2/exercises/statistics_oop/schemas/statistics.py
@@ -1,10 +1,28 @@
+from operator import itemgetter
+
+
 class Statistics:
     @classmethod
     def mean(cls, numbers):
         return sum(numbers) / len(numbers)
 
-    def median(cls):
-        pass
+    @classmethod
+    def median(cls, numbers):
+        if len(numbers) % 2 == 0:
+            n1 = int(len(numbers) / 2 - 1)
+            n2 = int(len(numbers) / 2)
+            return (numbers[n1] + numbers[n2]) / 2
+        else:
+            return numbers[int(len(numbers) / 2)]
 
-    def modal(cls):
-        pass
+    @classmethod
+    def modal(cls, numbers):
+        modal = {}
+        for number in numbers:
+            if number in modal.keys():
+                modal[number] += 1
+            else:
+                modal[number] = 1
+
+        tuples = modal.items()
+        return sorted(tuples, key=itemgetter(1), reverse=True)[0][0]
