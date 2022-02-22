0d968ded09c3bdc8eebfbef20378358659008238
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-22 18:22:20 -0300

    day 33.2 - exercises 2 - mean

---
+++ bloco_33/day_2/exercises/statistics_oop/main.py
@@ -0,0 +1,5 @@
+from schemas.statistics import Statistics
+
+n_list = [1, 2, 3, 4]
+
+print('mean =>', Statistics.mean(n_list))
---
+++ bloco_33/day_2/exercises/statistics_oop/schemas/statistics.py
@@ -0,0 +1,10 @@
+class Statistics:
+    @classmethod
+    def mean(cls, numbers):
+        return sum(numbers) / len(numbers)
+
+    def median(cls):
+        pass
+
+    def modal(cls):
+        pass
