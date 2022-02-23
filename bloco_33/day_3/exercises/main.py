2a0d76e36bb47e8b958438fc50fe0c393c10142e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-23 18:46:34 -0300

    day 33.3 - exercises 2 - cards

--- bloco_33/day_3/exercises/main.py
+++ bloco_33/day_3/exercises/main.py
@@ -1,4 +1,9 @@
-from schemas.star_wars import StarWarsGame, Soldier, Jedi
+# from schemas.star_wars import StarWarsGame, Soldier, Jedi
+from schemas.cards import Baralho
 
-StarWarsGame(Soldier(5)).fight_enemy()
-StarWarsGame(Jedi(20)).fight_enemy()
+# StarWarsGame(Soldier(5)).fight_enemy()
+# StarWarsGame(Jedi(20)).fight_enemy()
+cartas = Baralho()
+
+for carta in cartas:
+    print(carta)
