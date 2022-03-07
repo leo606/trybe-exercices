5e1b765e36d09a569f747956c209b4fd1e64a12a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-07 17:55:44 -0300

    day 34.3 - content 4

--- bloco_34/day_3/content/python_scraping/crawler.py
+++ bloco_34/day_3/content/python_scraping/crawler.py
@@ -1,7 +1,12 @@
 import requests
-import time
 
-for _ in range(15):
-    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+
+try:
+    response = requests.get("https://httpbin.org/delay/10", timeout=3)
+    print(response)
+except requests.ReadTimeout:
+    print("except")
+    response = requests.get("https://httpbin.org/delay/1", timeout=2)
+    print(response)
+finally:
     print(response.status_code)
-    time.sleep(6)
