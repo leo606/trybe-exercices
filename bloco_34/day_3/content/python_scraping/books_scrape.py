3cdd61f8cb8668696f80823f01e313a192db01b1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-07 19:00:07 -0300

    day 34.3 - content 5

---
+++ bloco_34/day_3/content/python_scraping/books_scrape.py
@@ -0,0 +1,38 @@
+import requests
+from parsel import Selector
+
+# response = requests.get("http://books.toscrape.com/")
+# selector = Selector(text=response.text)
+
+# # O título está no atributo title em um elemento âncora (<a>)
+# # Dentro de um h3 em elementos que possuem classe product_pod
+# titles = selector.css(".product_pod h3 a::attr(title)").getall()
+# # Estamos utilizando a::attr(title) para capturar somente o valor contido no
+# # texto do seletor
+
+# # Os preços estão no texto de uma classe price_color
+# # Que se encontra dentro da classe .product_price
+# prices = selector.css(
+#     ".product_pod .product_price p.price_color::text"
+# ).getall()
+
+# # Combinando tudo podemos buscar os produtos
+# # em em seguida buscar os valores individualmente
+# for product in selector.css(".product_pod"):
+#     title = product.css("h3 a::attr(title)").get()
+#     price = product.css(".product_price p.price_color::text").get()
+#     print(title, price)
+
+BASE_URL = "https://books.toscrape.com/catalogue/"
+next_page_url = "page-1.html"
+
+while next_page_url:
+    # Busca o conteúdo da próxima página
+    response = requests.get(BASE_URL + next_page_url)
+    selector = Selector(text=response.text)
+    # Imprime os produtos de uma determinada página
+    for product in selector.css(".product_pod"):
+        title = product.css("h3 a::attr(title)").get()
+        price = product.css(".product_price p.price_color::text").get()
+        print(title, price)
+    next_page_url = selector.css(".next a::attr(href)").get()
