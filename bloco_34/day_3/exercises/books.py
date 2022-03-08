2778f33232be5bf49122e52e53af04dd42348c30
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-08 20:55:47 -0300

    day 34.3 - exercise 4

---
+++ bloco_34/day_3/exercises/books.py
@@ -0,0 +1,34 @@
+import requests
+from parsel import Selector
+
+# Exercício 4 Baseados em uma página contendo detalhes sobre um
+# livro http://books.toscrape.com/catalogue/the-grand-design_405/index.html ,
+# faça a extração dos campos título, preço,
+# descrição e url contendo a imagem de capa do livro.
+# O preço deve vir somente valores numéricos e ponto. Ex: Â£13.76 -> 13.76 .
+# A descrição de ter o sufixo "more..." removido quando existir.
+# Os campos extraídos devem ser apresentados separados por vírgula.
+# Ex: título,preço,descrição,capa.
+
+URL_BASE = (
+    "https://books.toscrape.com/catalogue/the-grand-design_405/index.html"
+)
+description_suffix = "...more"
+
+response = requests.get(URL_BASE)
+selector = Selector(response.text)
+
+title = selector.css(".product_main h1::text").get()
+
+price = selector.css(".product_main .price_color::text").re_first(
+    r"\d+\.\d{2}"
+)
+
+description = selector.css(
+    ".product_page #product_description ~ p::text"
+).get()
+
+if description.endswith(description_suffix):
+    description = description.removesuffix(description_suffix)
+
+print(f"{title}, {price}, {description}")
