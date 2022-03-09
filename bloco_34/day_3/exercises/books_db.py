dab98461cff3ba40be5bcd58df1bd7eb6f2561a4
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-09 09:24:26 -0300

    day 34.3 - exercise 7

--- bloco_34/day_3/exercises/books_db.py
+++ bloco_34/day_3/exercises/books_db.py
@@ -17,4 +17,26 @@ def list_by_category(category):
             pprint(book)
 
 
-list_by_category("Miscellaneous")
+# list_by_category("Miscellaneous")
+
+# Exercício 7 Faça o calculo de quantos livros publicados ( STATUS = PUBLISH )
+# temos em nosso banco de dados por categoria. Ordenando-os de forma
+# decrescente de acordo com a quantidade.
+
+
+def list_published():
+    with MongoClient() as client:
+        db = client.library
+        books = db.books.aggregate(
+            [
+                {"$match": {"status": "PUBLISH"}},
+                {"$unwind": "$categories"},
+                {"$group": {"_id": "$categories", "quantity": {"$count": {}}}},
+                {"$sort": {"quantity": -1}},
+            ]
+        )
+        for book in books:
+            pprint(f"{book['_id']} {book['quantity']}")
+
+
+list_published()
