47d03f2bdefaf2b46e79b7cb1243f1db81129706
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-15 15:50:15 -0300

    day 35.1 - exercise 3

---
+++ bloco_35/day_1/exercises/numbers.js
@@ -0,0 +1,7 @@
+// Exercício 3 O código abaixo está em JavaScript.
+// Calcule sua ordem de complexidade para um array de tamanho n.
+
+// ordem de complexidade: O(n)
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.map((n) => n * n);
