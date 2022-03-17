c41d3647b56697fbc5502d4d169c62c6de36ff09
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-17 09:16:10 -0300

    day 35.2 - exercise 2

--- bloco_35/day_2/exercises/even_number.py
+++ bloco_35/day_2/exercises/even_number.py
@@ -1,11 +1,21 @@
 def even_numbers(n):
     pares = 0
 
-    for number in range(1, n):
+    for number in range(n):
         if number % 2 == 0:
             pares += 1
 
     return pares
 
 
-print(even_numbers(5))
+def even_numbers_rec(n):
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + even_numbers_rec(n - 1)
+    else:
+        return even_numbers_rec(n - 1)
+
+
+print(even_numbers(6))
+print(even_numbers_rec(6))
