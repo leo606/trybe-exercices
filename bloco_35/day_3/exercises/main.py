4138bd70c031da46c84799b070e75cbf59046b83
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-17 14:48:51 -0300

    timer with

--- bloco_35/day_3/exercises/main.py
+++ bloco_35/day_3/exercises/main.py
@@ -1,7 +1,9 @@
 # from merge_sort import merge_sort
 from quick_sort import quicksort
+from Timer import Timer
 
 if __name__ == "__main__":
-    array = [100, 4, 6, 33, 56, 67]
-    quicksort(array, 0, len(array) - 1)
-    print(array)
+    with Timer("quick sort",):
+        array = [100, 4, 6, 33, 56, 67]
+        quicksort(array, 0, len(array) - 1)
+        print(array)
