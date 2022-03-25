86c831e92290e44a1dd2e7314ec395ac39b44ac0
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-25 10:02:41 -0300

    day 36.2 - content 2

--- bloco_36/day_2/content/array_example.py
+++ bloco_36/day_2/content/array_example.py
@@ -1,3 +1,5 @@
+import sys
+
 """Perceba que temos uma coleção de valores
 e operações que atuam sobre estes valores,
 de acordo com o que foi definido pelo TAD."""
@@ -22,23 +24,37 @@ class Array:
     def set(self, index, value):
         self.data.insert(index, value)
 
+    def remove(self, index):
+        # removeremos o item, retornando-o
+        return self.data.pop(index)
 
-# vamos inicializar e preencher uma estrutura de dados array
+    def update(self, index, value):
+        self.data[index] = value
+
+
 array = Array()
-array.set(0, "Felipe")
-array.set(1, "Ana")
-array.set(2, "Shirley")
-array.set(3, "Miguel")
+array_memory_size = sys.getsizeof(array.data)
+print("empty array memory - ", array_memory_size)
 
-# para acessar um elemento do array, utilizamos seu índice
-print(array.get(0))  # saída: Felipe
-print(array.get(2))  # saída: Shirley
-print("-----")
+array.set(0, "Marcos")
+array.set(1, "Patrícia")
 
-# podemos iterar sobre seus elementos da seguinte maneira
-index = 0
-# enquanto há elementos no array
-while index < len(array):
-    # recupera o elemento através de um índice
-    print("Index:", index, ", Nome:", array.get(index))
-    index += 1
+# quando começamos as inserções o valor muda
+array_memory_size = sys.getsizeof(array.data)
+print("two items memory - ", array_memory_size)
+
+array.set(2, "Matheus")
+array.set(3, "Giovana")
+array_memory_size = sys.getsizeof(array.data)
+print("four items memory - ", array_memory_size)
+
+array.set(4, "Alberto")
+array.set(5, "Marta")
+array.set(6, "Túlio")
+array.set(7, "Michelle")
+array_memory_size = sys.getsizeof(array.data)
+print("eight items memory - ", array_memory_size)
+
+print('idx 3', array.get(3))
+array.update(3, 'new value')
+print('idx 3', array.get(3))
