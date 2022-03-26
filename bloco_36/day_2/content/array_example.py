abd4c8c64056eceb6f70b5f40af2944aff5df2a1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-26 08:56:11 -0300

    day 36.2 - content 3

--- bloco_36/day_2/content/array_example.py
+++ bloco_36/day_2/content/array_example.py
@@ -1,5 +1,3 @@
-import sys
-
 """Perceba que temos uma coleção de valores
 e operações que atuam sobre estes valores,
 de acordo com o que foi definido pelo TAD."""
@@ -30,31 +28,3 @@ class Array:
 
     def update(self, index, value):
         self.data[index] = value
-
-
-array = Array()
-array_memory_size = sys.getsizeof(array.data)
-print("empty array memory - ", array_memory_size)
-
-array.set(0, "Marcos")
-array.set(1, "Patrícia")
-
-# quando começamos as inserções o valor muda
-array_memory_size = sys.getsizeof(array.data)
-print("two items memory - ", array_memory_size)
-
-array.set(2, "Matheus")
-array.set(3, "Giovana")
-array_memory_size = sys.getsizeof(array.data)
-print("four items memory - ", array_memory_size)
-
-array.set(4, "Alberto")
-array.set(5, "Marta")
-array.set(6, "Túlio")
-array.set(7, "Michelle")
-array_memory_size = sys.getsizeof(array.data)
-print("eight items memory - ", array_memory_size)
-
-print('idx 3', array.get(3))
-array.update(3, 'new value')
-print('idx 3', array.get(3))
