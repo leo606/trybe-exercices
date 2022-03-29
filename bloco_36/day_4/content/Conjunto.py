eb25d600dd1727577352074ce458a67cd4f49dd1
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-29 14:30:31 -0300

    day 36.4 - content 2

--- bloco_36/day_4/content/Conjunto.py
+++ bloco_36/day_4/content/Conjunto.py
@@ -4,3 +4,13 @@ class Conjunto:
 
     def add(self, value):
         self._set[value] = True
+
+    def __str__(self):
+        values = "{"
+        for index, value in enumerate(self._set):
+            if value:
+                values += str(index)
+                if index < len(self._set) - 1:
+                    values += ", "
+        values += "}"
+        return values
--- bloco_36/day_4/content/main.py
+++ bloco_36/day_4/content/main.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
 
     for value in values:
         set.add(value)
+
+    print(set)
